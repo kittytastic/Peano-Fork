71c1a6783867dd518ece0942e42a3514bed181d2
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2015-10-14 19:07:16 +0000

    Fixed bug/added missing implementation
    git-svn-id: https://svn.code.sf.net/p/peano/code/trunk@1934 aa977e5b-e928-4c84-aa66-c90e40304b12

---
+++ cookbook/chapter-4.1/myproject/Cell.cpp
@@ -0,0 +1,21 @@
+#include "myproject/Cell.h"
+
+
+myproject::Cell::Cell():
+  Base() { 
+  _cellData.setEpsilon( 1.0 + static_cast<double>(rand() % 100)/100.0 );
+}
+
+double myproject::Cell::getEpsilon() const {
+  return _cellData.getEpsilon();
+}
+
+myproject::Cell::Cell(const Base::DoNotCallStandardConstructor& value):
+  Base(value) { 
+  // Please do not insert anything here
+}
+
+myproject::Cell::Cell(const Base::PersistentCell& argument):
+  Base(argument) {
+  // @todo Insert your code here
+}
