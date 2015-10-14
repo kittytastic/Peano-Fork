50d4ff2b4d9bc19d8f5776e51b7964282f685a85
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2015-10-14 19:23:24 +0000

    Added
    git-svn-id: https://svn.code.sf.net/p/peano/code/trunk@1937 aa977e5b-e928-4c84-aa66-c90e40304b12

---
+++ cookbook/chapter-4.1/myproject/Vertex.cpp
@@ -0,0 +1,25 @@
+#include "myproject/Vertex.h"
+#include "peano/utils/Loop.h"
+#include "peano/grid/Checkpoint.h"
+
+
+myproject::Vertex::Vertex():
+  Base() { 
+  // @todo Insert your code here
+}
+
+
+myproject::Vertex::Vertex(const Base::DoNotCallStandardConstructor& value):
+  Base(value) { 
+  // Please do not insert anything here
+}
+
+
+myproject::Vertex::Vertex(const Base::PersistentVertex& argument):
+  Base(argument) {
+  // @todo Insert your code here
+}
+
+double myproject::Vertex::getU() const {
+  return vertexData.getU();
+}
