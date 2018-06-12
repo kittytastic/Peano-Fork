a99d4bddf5e7c2f5ac939f61f69f8639b5638000
Philipp Samfass <samfass@in.tum.de>
2018-06-12 15:14:11 +0200

    added some more files

---
+++ daily-build.sh
@@ -0,0 +1,11 @@
+folders=(pdt src visualisation)
+files=(README.md)
+
+peanoVersion="Peano-$(git log --format="%h" -n 1 .)"
+
+tarName="$peanoVersion.tar.gz"
+
+echo $tarName
+
+tar --exclude-vcs --exclude=*.o -czvf  $tarName ${folders[*]} ${files[*]}
+
