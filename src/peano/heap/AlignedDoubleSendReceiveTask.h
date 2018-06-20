1604b722a201d33e8cdde674dea4d7f6ae7ff8d4
tobiasweinzierl <tobias.weinzierl@durham.ac.uk>
2018-06-20 10:20:33 +0100

    minor fixes of missing deletes and better/consistent comments

--- src/peano/heap/AlignedDoubleSendReceiveTask.h
+++ src/peano/heap/AlignedDoubleSendReceiveTask.h
@@ -26,22 +26,11 @@ namespace peano {
 
 
 /**
- * Wrapper for a send or receive task
- *
- * Holds the information for a send or receive task, i.e. it holds the MPI
- * request to check weather the task has been finished (both sends and
- * receives are realised as non-blocking MPI data exchange) as well as a
- * pointer to the allocated data where the copy of the data is
- * stored (send task) or is to be stored (receive task).
+ * @see Generic (template) class. This is a specialisation with the same semantics.
  */
 template<int Alignment>
 class peano::heap::AlignedDoubleSendReceiveTask {
   public:
-    /**
-     * We always use the plain meta information as record, i.e. we do not pack
-     * anything here as the meta information usually is one integer only
-     * anyway.
-     */
     typedef peano::heap::records::MetaInformation          MetaInformation;
 
   private:
@@ -71,55 +60,12 @@ class peano::heap::AlignedDoubleSendReceiveTask {
   public:
     AlignedDoubleSendReceiveTask();
 
-  /**
-   * Prelude to sendData().
-   *
-   * Please note that you have to call delete[] on _data afterwards through
-   * operation freeMemory().
-   */
-  void wrapData(const double* const data);
-
-  /**
-   * Counterpart of wrapData(). The task sends away the data directly from the
-   * specified buffer. Please call freeMemory() nevertheless.
-   */
-  void sendDataDirectlyFromBuffer(const double* const data);
-
-  /**
-   * @see triggerReceive() for implementation remarks.
-   */
-  void triggerSend(int tag);
-
-  /**
-   * If you use the task in combination with containers, please push or pop
-   * data first and then call trigger. Trigger uses MPI and MPI uses memory
-   * references. If you move the tasks around later on, MPI references invalid
-   * memory addresses and data consistency is not given anymore.
-   */
-  void triggerReceive(int tag);
-
-  void freeMemory();
-
-  /**
-   * Set a task invalid explicitly. Messages marked that way will pass the
-   * validation though their data is not in agreement with checks: it is
-   * explicilty known that the message is invalid and can be ignored. I use
-   * this for null messages, i.e. messages without content that are often
-   * squeezed (together with their meta data) by sophisticated communication
-   * schemes.
-   */
-  void setInvalid();
-
-    /**
-     * A task fits if it is
-     *
-     * - either invalid (see setInvalid())
-     * - or position and level coincide.
-     *
-     * Fits should only be called in assert mode. However, some compiler seem to
-     * translate it also if the function is not used at all. For them, I provide
-     * a non-asserts version returning true all the time.
-     */
+    void wrapData(const double* const data);
+    void sendDataDirectlyFromBuffer(const double* const data);
+    void triggerSend(int tag);
+    void triggerReceive(int tag);
+    void freeMemory();
+    void setInvalid();
     bool fits(
       const tarch::la::Vector<DIMENSIONS, double>&  position,
       int                                           level
