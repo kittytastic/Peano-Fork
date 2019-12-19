210154a6adb8d4e1170e85ba9a380b0e0aa9a145
hpcsoftware <hpcsoftware@hpcsoftware.dur.ac.uk>
2019-12-19 13:50:49 +0000

    3d setups are simply too small

--- src/examples/regulargridupscaling/local-workstation/shared-memory-3d.sh
+++ src/examples/regulargridupscaling/local-workstation/shared-memory-3d.sh
@@ -3,14 +3,14 @@
 export d=3
 for flops in 1 100 
 do
- for h in 0.5 0.2 0.1
+ for h in 0.05 0.02 0.01
  do
   for cores in 1 2 3 4 6 8 10 12 14 16 20 24 48
   do
    executable="./PeanoRegularGridUpscaling"$d"d"
    output="shared-memory-"$cores"-cores-"$d"d-"$h"-"$flops"-flops.out"
    echo $executable  $output
-   timeout 60m $executable $h $flops $cores > $output
+   timeout 120m $executable $h $flops $cores > $output
   done
  done
 done
