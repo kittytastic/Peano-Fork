2c2640efa7fcf5788e9fc84a3a00360ff620d3cb
hpcsoftware <hpcsoftware@hpcsoftware.dur.ac.uk>
2019-12-27 16:59:42 +0000

    make large runs only run parallel

--- src/examples/regulargridupscaling/local-workstation/shared-memory-3d.sh
+++ src/examples/regulargridupscaling/local-workstation/shared-memory-3d.sh
@@ -16,4 +16,18 @@ do
 done
 
 
+for flops in 1 100
+do
+ for h in 0.01 
+ do
+  for cores in 4 6 8 10 12 14 16 20 24 48
+  do
+   executable="./PeanoRegularGridUpscaling"$d"d"
+   output="shared-memory-"$cores"-cores-"$d"d-"$h"-"$flops"-flops.out"
+   echo $executable  $output
+   timeout 120m $executable $h $flops $cores > $output
+  done
+ done
+done
+
 
