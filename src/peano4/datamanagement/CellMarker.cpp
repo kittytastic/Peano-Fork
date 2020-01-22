4d6924331c62076c7bf645a6a0b25aa779dfa33e
tobias <tobias@Bad>
2020-01-22 16:06:16 +0000

    Removed cell marker, too, even though there's a void specialisation of this one if no cell data is used.

--- src/peano4/datamanagement/CellMarker.cpp
+++
@@ -1,9 +0,0 @@
-#include "CellMarker.h"
-#include "peano4/grid/GridTraversalEvent.h"
-
-
-peano4::datamanagement::CellMarker::CellMarker(const peano4::grid::GridTraversalEvent&  event):
-  isRefined( event.getIsRefined() ),
-  isAdjacentToTreeBoundary(false) {
-}
-
