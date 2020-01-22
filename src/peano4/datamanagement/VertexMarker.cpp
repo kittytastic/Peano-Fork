61ee57a18508cc9ca86f1eb0687147ca210d628b
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2020-01-22 18:10:01 +0000

    Not required anymore

--- src/peano4/datamanagement/VertexMarker.cpp
+++
@@ -1,7 +0,0 @@
-#include "VertexMarker.h"
-#include "peano4/grid/GridTraversalEvent.h"
-
-
-peano4::datamanagement::VertexMarker::VertexMarker(const peano4::grid::GridTraversalEvent&  event, int vertexNumber) {
-}
-
