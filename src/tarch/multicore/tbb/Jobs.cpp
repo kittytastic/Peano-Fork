f00eadbfe2f9c1847fd14b4c639bf2b032047d75
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2020-01-29 13:50:52 +0000

    Fixed bool

--- src/tarch/multicore/tbb/Jobs.cpp
+++ src/tarch/multicore/tbb/Jobs.cpp
@@ -135,11 +135,11 @@ tbb::task* tarch::multicore::jobs::internal::JobConsumerTask::execute() {
 
 
   if ( _bandwidthTasksAreProcessed.compare_and_swap(true,false) ) {
-    hasProcessedJobs |= processJobs(internal::HighBandwidthTasksJobClassNumber,std::numeric_limits<int>::max(), -1, true);
+    hasProcessedJobs |= processJobs(internal::HighBandwidthTasksJobClassNumber,std::numeric_limits<int>::max(), -1, false);
     _bandwidthTasksAreProcessed = false;
   }
 
-  hasProcessedJobs |= processJobs(internal::BackgroundTasksJobClassNumber,_maxJobs, -1, true);
+  hasProcessedJobs |= processJobs(internal::BackgroundTasksJobClassNumber,_maxJobs, -1, false);
 
   if ( hasProcessedJobs or rescheduleThisConsumer ) {
     enqueue();
