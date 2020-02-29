b6856372244e2fa4db872e8bd942bd629be3aa6e
tobias <tobias@Bad>
2020-02-29 15:12:30 +0000

    Extracted program logic into class of its own. On the long term, I want to provide a Python API for this. But it serves as a nice blueprint.

--- python/examples/jacobi/amr-parallel-main.cpp
+++ python/examples/jacobi/amr-parallel-main.cpp
@@ -28,7 +28,124 @@
 #include "observers/FusedSolverSteps.h"
 
 
-tarch::logging::Log _log("::");
+tarch::logging::Log _log("");
+
+
+class ProgramRun {
+  private:
+  public:
+    /**
+     * Init spacetree set
+     */
+    ProgramRun() {
+      peano4::parallel::SpacetreeSet::getInstance().init(
+        examples::jacobi::actions::SetupScenario::getDomainOffset(),
+        examples::jacobi::actions::SetupScenario::getDomainSize()
+      );
+    }
+
+
+    void step() {
+      int stepIdentifier = peano4::parallel::Node::getInstance().getCurrentProgramStep();
+      logInfo( "traverseGrid(...)", "run step " << stepIdentifier );
+
+      // @todo Hier waere ein Case schoen
+      switch (stepIdentifier) {
+        case 0:
+          {
+            examples::jacobi::observers::CreateGrid  observer;
+            peano4::parallel::SpacetreeSet::getInstance().traverse(observer);
+          }
+          break;
+        case 1:
+          {
+            examples::jacobi::observers::SetupScenario  observer;
+            peano4::parallel::SpacetreeSet::getInstance().traverse(observer);
+          }
+          break;
+        case 2:
+          {
+            examples::jacobi::observers::PlotMaterialParameter  observer;
+            peano4::parallel::SpacetreeSet::getInstance().traverse(observer);
+          }
+          break;
+        case 10:
+          {
+            examples::jacobi::observers::FusedSolverSteps  observer;
+            peano4::parallel::SpacetreeSet::getInstance().traverse(observer);
+          }
+          break;
+        case 99:
+          {
+            examples::jacobi::observers::PlotSolution  observer;
+            peano4::parallel::SpacetreeSet::getInstance().traverse(observer);
+          }
+          break;
+        default:
+          assertionMsg(false, "step unknown");
+      }
+
+      tarch::logging::CommandLineLogger::getInstance().closeOutputStreamAndReopenNewOne();
+    }
+
+    void runGlobalMaster(int iterations) {
+      // Construct grid until we are told that it hasn't changed for
+      // more than two iterations.
+      // ===========================================================
+      do {
+        peano4::parallel::Node::getInstance().setNextProgramStep(0);
+        step();
+      }
+      while (peano4::parallel::SpacetreeSet::getInstance().getGridStatistics().getStationarySweeps()<2);
+
+      peano4::parallel::Node::getInstance().setNextProgramStep(1); // setup scenario
+      step();
+
+      peano4::parallel::Node::getInstance().setNextProgramStep(2); // dump parameters
+      step();
+
+      // Split up domain among ranks
+      // ===========================
+/*
+      if ( tarch::mpi::Rank::getInstance().getNumberOfRanks()>1 ) {
+        int cellsPerRank = peano4::parallel::SpacetreeSet::getInstance().getGridStatistics().getNumberOfLocalUnrefinedCells() / tarch::mpi::Rank::getInstance().getNumberOfRanks();
+        logInfo( "main()", "should host " << cellsPerRank << " cells per rank (total number of cells: " << peano4::parallel::SpacetreeSet::getInstance().getGridStatistics().getNumberOfLocalUnrefinedCells() << ")" );
+
+        for (int rank=1; rank<tarch::mpi::Rank::getInstance().getNumberOfRanks(); rank++) {
+          if ( not peano4::parallel::SpacetreeSet::getInstance().split(0,cellsPerRank,rank)) {
+            logWarning( "runParallel(...)", "failed to assign rank " << rank << " " << cellsPerRank << " cell(s)" );
+          }
+        }
+      }
+*/
+
+      // Split up domain among threads
+      // ===========================================================
+/*
+      if (coreCount>1) {
+        int cellsPerCore = peano4::parallel::SpacetreeSet::getInstance().getGridStatistics().getNumberOfLocalUnrefinedCells() / coreCount;
+        logInfo( "main()", "should host " << cellsPerCore << " cells per core" );
+
+        for (int thread=1; thread<coreCount; thread++) {
+          if ( not peano4::parallel::SpacetreeSet::getInstance().split(0,cellsPerCore,0)) {
+            logWarning( "runParallel(...)", "failed to assign thread " << thread << " " << cellsPerCore << " cell(s)" );
+          }
+        }
+      }
+*/
+
+      for (int i=0; i<iterations; i++) {
+        peano4::parallel::Node::getInstance().setNextProgramStep(10); // solve problem
+        step();
+      }
+
+      peano4::parallel::Node::getInstance().setNextProgramStep(99); // plot solution
+      step();
+    }
+
+
+};
+
 
 
 int main(int argc, char** argv) {
@@ -128,67 +245,15 @@ int main(int argc, char** argv) {
   }
 
 
-  peano4::parallel::SpacetreeSet::getInstance().init(
-    examples::jacobi::actions::SetupScenario::getDomainOffset(),
-    examples::jacobi::actions::SetupScenario::getDomainSize()
-  );
+  ProgramRun programRun;
   
   if (tarch::mpi::Rank::getInstance().isGlobalMaster() ) {
-    // Construct grid until we are told that it hasn't changed for 
-    // more than two iterations.
-    // ===========================================================
-    do {
-      examples::jacobi::observers::CreateGrid  observer;
-      peano4::parallel::SpacetreeSet::getInstance().traverse(observer);
-    }
-    while (peano4::parallel::SpacetreeSet::getInstance().getGridStatistics().getStationarySweeps()<2);
-
-    // Setup mesh and dump material parameter epsilon
-    // ===========================================================
-    {
-      examples::jacobi::observers::SetupScenario  setupScenario;
-      peano4::parallel::SpacetreeSet::getInstance().traverse(setupScenario);
-
-      examples::jacobi::observers::PlotMaterialParameter  plotMaterialParameter;
-      peano4::parallel::SpacetreeSet::getInstance().traverse(plotMaterialParameter);
-
-      tarch::logging::CommandLineLogger::getInstance().closeOutputStreamAndReopenNewOne();
-    }
-
-    // Split up domain among threads
-    // ===========================================================
-    if (coreCount>1) {
-      int cellsPerCore = peano4::parallel::SpacetreeSet::getInstance().getGridStatistics().getNumberOfLocalUnrefinedCells() / coreCount;
-      logInfo( "main()", "should host " << cellsPerCore << " cells per core" );
-
-      for (int thread=1; thread<coreCount; thread++) {
-        if ( not peano4::parallel::SpacetreeSet::getInstance().split(0,cellsPerCore,0)) {
-          logWarning( "runParallel(...)", "failed to assign thread " << thread << " " << cellsPerCore << " cell(s)" );
-        }
-      }
-    }
-    
-    // Solve problem
-    // ===========================================================
-    const int MaxIterations = 80;
-
-    for (int i=0; i<MaxIterations; i++)
-    {
-      examples::jacobi::observers::FusedSolverSteps observer;
-      peano4::parallel::SpacetreeSet::getInstance().traverse(observer);
-      tarch::logging::CommandLineLogger::getInstance().closeOutputStreamAndReopenNewOne();
-    }
-
-    // Plot output
-    // ===========================================================
-    {
-      examples::jacobi::observers::PlotSolution  observer;
-      peano4::parallel::SpacetreeSet::getInstance().traverse(observer);
-    }
+    const int MaxIterations = 10;
+    programRun.runGlobalMaster(MaxIterations);
   }
   else {
     while (peano4::parallel::Node::getInstance().continueToRun()) {
-      logDebug( "runParallel(...)", "trigger a new sweep with step " << peano4::parallel::Node::getInstance().getCurrentProgramStep() );
+      programRun.step();
     }
   }
 
