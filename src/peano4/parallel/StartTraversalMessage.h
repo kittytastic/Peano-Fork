1e3d77711bac058e02f938ac0cc41d2a0fe4e5ff
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2020-03-13 13:34:54 +0000

    Regenerated all the datatypes once again. This time without any MPI_C_BOOL as this one doesn't seem to work either.

--- src/peano4/parallel/StartTraversalMessage.h
+++ src/peano4/parallel/StartTraversalMessage.h
@@ -28,7 +28,7 @@ namespace peano4 {
  *
  * 		   build date: 09-02-2014 14:40
  *
- * @date   13/03/2020 13:09
+ * @date   13/03/2020 13:34
  */
 class peano4::parallel::StartTraversalMessage { 
    
@@ -157,7 +157,7 @@ class peano4::parallel::StartTraversalMessage {
  *
  * 		   build date: 09-02-2014 14:40
  *
- * @date   13/03/2020 13:09
+ * @date   13/03/2020 13:34
  */
 class peano4::parallel::StartTraversalMessagePacked { 
    
