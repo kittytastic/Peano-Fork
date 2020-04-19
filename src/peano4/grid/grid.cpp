544d6410c0decbca21f2c96427453ff5dc2578d0
tobias <tobias@Bad>
2020-04-19 14:36:50 +0100

    Consolidated stats merges and resets. Operations are now real opertors and not functions.

--- src/peano4/grid/grid.cpp
+++ src/peano4/grid/grid.cpp
@@ -1,1 +1,25 @@
 #include "grid.h"
+#include "GridStatistics.h"
+
+
+peano4::grid::GridStatistics operator+( peano4::grid::GridStatistics lhs, peano4::grid::GridStatistics rhs ) {
+  return peano4::grid::GridStatistics(
+    lhs.getNumberOfLocalUnrefinedCells()  + rhs.getNumberOfLocalUnrefinedCells(),
+    lhs.getNumberOfRemoteUnrefinedCells() + rhs.getNumberOfRemoteUnrefinedCells(),
+    lhs.getNumberOfLocalRefinedCells()  + rhs.getNumberOfLocalRefinedCells(),
+    lhs.getNumberOfRemoteRefinedCells() + rhs.getNumberOfRemoteRefinedCells(),
+    std::min(lhs.getStationarySweeps(),rhs.getStationarySweeps()),
+    lhs.getCoarseningHasBeenVetoed() | rhs.getCoarseningHasBeenVetoed()
+  );
+}
+
+
+void peano4::grid::clear( GridStatistics& statistics, bool isGlobalMasterTree ) {
+  statistics.setNumberOfLocalUnrefinedCells(0);
+  statistics.setNumberOfRemoteUnrefinedCells(0);
+  statistics.setNumberOfLocalRefinedCells(isGlobalMasterTree ? 1 : 0);
+  statistics.setNumberOfRemoteRefinedCells(isGlobalMasterTree ? 0 : 1);
+  statistics.setCoarseningHasBeenVetoed(false);
+  statistics.setStationarySweeps( statistics.getStationarySweeps()+1 );
+}
+
