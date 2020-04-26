a26b2d26484c7bad820ba90e4cb52e98a019ed8e
tobias <tobias@Bad>
2020-04-26 10:18:02 +0100

    First default implementation of patch boundary exchange between MPI boundaries

--- python/peano4/toolbox/blockstructured/MPI.py
+++ python/peano4/toolbox/blockstructured/MPI.py
@@ -27,10 +27,27 @@ def get_face_overlap_merge_implementation(patch_overlap):
     return result;
   }};
   
-  dfore(i,{DOFS_PER_AXIS},marker.normalAxis(),0) {{
+  //
+  // The face merges are always called from a cell. So it is one particular
+  // cell that loads a particular face for the first time and thus 
+  //
+  //
+  #if PeanoDebug>0
+  tarch::logging::Log _log( "Q" );
+  logDebug( "mergeHorizontally()", "merge " << marker.toString() ); 
+  #endif
+  
+  const int faceNormal = marker.getSelectedFaceNumber() % Dimensions;
+  dfore(i,{DOFS_PER_AXIS},faceNormal,0) {{
     for (int j=0; j<{OVERLAP}; j++) {{
+      tarch::la::Vector<Dimensions,int> volume = i;
+      volume(faceNormal) += marker.outerNormal()(faceNormal)>0 ? j + {OVERLAP} : j;
+      
+      int volumeSerialised = serialisePatchIndex(volume, faceNormal);
+      value[volumeSerialised] = neighbour.value[volumeSerialised];
+      
+      assertion(value[volumeSerialised]==value[volumeSerialised]);
     }}
-    // marker.getSelectedFaceNumber()<Dimensions ? 0 : 1
   }}
 """
 
