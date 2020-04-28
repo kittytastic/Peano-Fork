3f7e9990fd7218e7bde7742b82b12d4c55b21ba9
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2020-04-28 19:35:58 +0100

    Better don't use my dimension-generic solver, as I'm afraid it is buggy

--- python/examples/exahype2/finitevolumes/finitevolumes-with-ExaHyPE2-parallel.py
+++ python/examples/exahype2/finitevolumes/finitevolumes-with-ExaHyPE2-parallel.py
@@ -46,7 +46,7 @@ project = exahype2.Project( ["examples", "exahype2", "finitevolumes"], "finitevo
 patch_size     = 25
 unknowns       = 5
 time_step_size = 0.001
-project.add_finite_volumes_solver("ParallelEuler", patch_size, unknowns, 0.001)
+project.add_finite_volumes_solver("ParallelEuler", patch_size, unknowns, 0.001, exahype2.solvers.FiniteVolumeSolverType.Rusanov_Edge)
 
 
 dimensions = 2
