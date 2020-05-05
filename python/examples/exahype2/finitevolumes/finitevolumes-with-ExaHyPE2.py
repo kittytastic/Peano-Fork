45f7f35f57993c122eee8f55df47524b0bd3555b
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2020-05-05 06:14:57 +0100

    Fixed issue that people might link debug version of code base to release version of executable. Also fixed issue that 2d kernels sometimes are selected for 3d build.

--- python/examples/exahype2/finitevolumes/finitevolumes-with-ExaHyPE2.py
+++ python/examples/exahype2/finitevolumes/finitevolumes-with-ExaHyPE2.py
@@ -72,7 +72,7 @@ else:
 
 peano4_project = project.generate_Peano4_project()
 peano4_project.output.makefile.parse_configure_script_outcome( "../../../.." )
-peano4_project.output.makefile.add_library( project.get_library(build_mode), "../../../../src/exahype2" )
+peano4_project.output.makefile.add_library( project.get_core_library(build_mode), "../../../../src/exahype2" )
 peano4_project.output.makefile.set_mode(build_mode)
 peano4_project.generate(peano4.output.Overwrite.Default)
 peano4_project.build()
