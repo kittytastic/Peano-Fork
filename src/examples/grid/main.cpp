d7942bede94eab0927f37c223ee00e606caa9385
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2020-05-16 13:25:26 +0100

    Use proper MPI abort introduced through Rank class.

--- src/examples/grid/main.cpp
+++ src/examples/grid/main.cpp
@@ -38,13 +38,7 @@ void runTests() {
 
   if (unitTestsErrors != 0) {
     logError("main()", "unit tests failed. Quit.");
-    std::cout.flush();
-    std::cerr.flush();
-    #if Parallel
-    MPI_Abort(MPI_COMM_WORLD,-2);
-    #else
-    exit(-2);
-    #endif
+    tarch::mpi::Rank::abort( -2 );
   }
 }
 
