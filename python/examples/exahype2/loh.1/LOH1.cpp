802df71107bcc30939d3b4f8c4d8e478067f9f8d
Dominic Etienne Charrier <dominic.charrier@googlemail.com>
2020-05-17 19:12:34 +0200

    Start working on this. Probably will have another chance to continue next weekend.

---
+++ python/examples/exahype2/loh.1/LOH1.cpp
@@ -0,0 +1,92 @@
+#include "LOH1.h"
+
+#include "PDE.h"
+#include "InitialData.h"
+
+tarch::logging::Log   examples::exahype2::finitevolumes::LOH1::_log( "examples::exahype2::finitevolumes::LOH1" );
+
+
+
+::exahype2::RefinementCommand examples::exahype2::finitevolumes::LOH1::refinementCriterion(
+  double Q[9],
+  const tarch::la::Vector<Dimensions,double>&  x,
+  const tarch::la::Vector<Dimensions,double>&  h,
+  double                                       t
+) {
+  logTraceInWith3Arguments( "refinementCriterion(...)", x, h, t );
+  ::exahype2::RefinementCommand result = ::exahype2::RefinementCommand::Keep;
+  if (tarch::la::equals(t,0.0) and tarch::la::max(h)>1.0/3.0 ) {
+    result = ::exahype2::RefinementCommand::Refine;
+  }
+  logTraceOutWith1Argument( "refinementCriterion(...)", toString(result) );
+  return result;
+}
+
+
+void examples::exahype2::finitevolumes::LOH1::adjustSolution(
+  double Q[9],
+  const tarch::la::Vector<Dimensions,double>&  x,
+  const tarch::la::Vector<Dimensions,double>&  h,
+  double                                       t
+) {
+  logTraceInWith3Arguments( "adjustSolution(...)", x, h, t );
+  if (tarch::la::equals(t,0.0) ) {
+    // initial conditions
+    double xd[Dimensions];
+    for (int d=0; d< Dimensions; d++) xd[d] = 0.0;//x[d];
+    alfenwave_(&xd[0], &Q[0], &t);
+    for(int i=0; i<9; i++){
+        if(!std::isfinite(Q[i])) std::cout << "xd " << xd[0] << " " << xd[1] << std::endl;
+        assert(std::isfinite(Q[i]));
+        assert(!std::isnan(Q[i]));
+    }
+  }
+  logTraceOut( "adjustSolution(...)" );
+}
+
+
+
+void examples::exahype2::finitevolumes::LOH1::eigenvalues(
+  double                                       Q[9],
+  const tarch::la::Vector<Dimensions,double>&  faceCentre,
+  const tarch::la::Vector<Dimensions,double>&  volumeH,
+  double                                       t,
+  int                                          normal,
+  double                                       lambda[9]
+) {
+  logTraceInWith4Arguments( "eigenvalues(...)", faceCentre, volumeH, t, normal );
+  double nv[3] = {0.};
+  nv[normal] = 1;
+  //pdeeigenvalues_(lambda, Q, nv);
+  logTraceOut( "eigenvalues(...)" );
+}
+
+
+void examples::exahype2::finitevolumes::LOH1::flux(
+  double                                       Q[9],
+  const tarch::la::Vector<Dimensions,double>&  faceCentre,
+  const tarch::la::Vector<Dimensions,double>&  volumeH,
+  double                                       t,
+  int                                          normal,
+  double                                       F[9]
+) {
+  logTraceInWith4Arguments( "flux(...)", faceCentre, volumeH, t, normal );
+  //pdeflux_(F, Q, &normal);
+  logTraceOut( "flux(...)" );
+}
+
+
+void examples::exahype2::finitevolumes::LOH1::boundaryConditions(
+  double                                       Qinside[9],
+  double                                       Qoutside[9],
+  const tarch::la::Vector<Dimensions,double>&  faceCentre,
+  const tarch::la::Vector<Dimensions,double>&  volumeH,
+  double                                       t,
+  int                                          normal
+) {
+  logTraceInWith4Arguments( "boundaryConditions(...)", faceCentre, volumeH, t, normal );
+  for(int i=0; i< 9; i++)
+      Qoutside[i]=Qinside[i];
+  logTraceOut( "boundaryConditions(...)" );
+}
+
---
+++ python/examples/exahype2/loh.1/README.md
@@ -0,0 +1,8 @@
+# LOH.1 in ExaHyPE2
+
+## Tasks
+
+* [ ] Check if NCP is supported by the existing FVM solver
+  * Answer:
+
+##  
