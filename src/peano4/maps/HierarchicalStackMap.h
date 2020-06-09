115e460d971023e5dd803bdfb9c9a29e82d5344b
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2020-06-09 16:56:17 +0100

    Switched to std map everywhere. Will reassess hierarchical map (which should be faster) later

--- src/peano4/maps/HierarchicalStackMap.h
+++ src/peano4/maps/HierarchicalStackMap.h
@@ -5,7 +5,10 @@
 
 
 #include "maps.h"
+
 #include "peano4/parallel/Node.h"
+
+#include "tarch/multicore/Lock.h"
 
 #include <map>
 
@@ -19,7 +22,9 @@ namespace peano4 {
 }
 
 // @todo Dokumentieren, warum ich denke, dass man das nicht braucht
-//#ifdef UseSemaphoreInHierarchicalStackMap
+// Also ich glaube jetzt doch, dass man das braucht
+// -> noe, das war's wohl net
+#define UseSemaphoreInHierarchicalStackMap
 
 
 /**
