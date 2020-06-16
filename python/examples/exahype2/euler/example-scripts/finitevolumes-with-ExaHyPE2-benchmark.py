fcb3ed955cd1ce5720e8f4344ec06800d5b91600
WEINZIERL <frmh84@hamilton2.hpc.dur.ac.uk>
2020-06-16 12:53:02 +0100

    fixed minor bugs in the scripts

--- python/examples/exahype2/euler/example-scripts/finitevolumes-with-ExaHyPE2-benchmark.py
+++ python/examples/exahype2/euler/example-scripts/finitevolumes-with-ExaHyPE2-benchmark.py
@@ -25,13 +25,6 @@ import peano4.toolbox.blockstructured
 import exahype2
 
 
-#
-# Lets first clean up all plot files, so we don't get a mismatch
-#
-output_files = [ f for f in os.listdir(".") if f.endswith(".peano-patch-file") or f.endswith(".vtu") or f.endswith( ".out") ]
-for f in output_files:
-  os.remove(f)
-
 
 #
 # Create a project and configure it to end up in a subnamespace (and thus
@@ -43,7 +36,7 @@ project = exahype2.Project( ["examples", "exahype2", "euler"], "finitevolumes", 
 #
 # Add the Finite Volumes solver
 #
-patch_size     = 11
+patch_size     = 7
 unknowns       = 5
 time_step_size = 0.000001
 
@@ -96,5 +89,5 @@ peano4_project.output.makefile.parse_configure_script_outcome( "../../../.." )
 peano4_project.output.makefile.add_library( project.get_core_library(build_mode), "../../../../src/exahype2" )
 peano4_project.output.makefile.add_library( "ToolboxLoadBalancing" + project.get_library_postfix(build_mode), "../../../../src/toolbox/loadbalancing" )
 peano4_project.output.makefile.set_mode(build_mode)
-peano4_project.build()
+peano4_project.build(make_clean_first=True)
 
