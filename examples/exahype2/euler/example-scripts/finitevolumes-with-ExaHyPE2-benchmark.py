3dc0e7be66d3d3a76f4e538d51cd0456e060a258
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2020-09-07 06:48:07 +0100

    Cleaned up experiment scripts
    Former-commit-id: 37b87a27030bf1272574948381527381e0b5d9eb

--- examples/exahype2/euler/example-scripts/finitevolumes-with-ExaHyPE2-benchmark.py
+++ examples/exahype2/euler/example-scripts/finitevolumes-with-ExaHyPE2-benchmark.py
@@ -15,16 +15,7 @@
 #
 import os
 import peano4
-import peano4.datamodel
-import peano4.solversteps
-import peano4.output
-import peano4.visualisation
-import peano4.toolbox.blockstructured
-
-
 import exahype2
-
-
 import argparse
 
 
@@ -48,6 +39,8 @@ project = exahype2.Project( ["examples", "exahype2", "euler"], "finitevolumes", 
 patch_size     = 7
 unknowns       = 5
 time_step_size = 0.000001
+min_h          = args.h
+max_h          = args.h
 
 #
 # Still the same solver, but this time we use named arguments. This is the way
@@ -57,7 +50,9 @@ time_step_size = 0.000001
 project.add_solver(  exahype2.solvers.GenericRusanovFVFixedTimeStepSizeWithEnclaves(
   "Euler", 
   patch_size, 
-  unknowns, time_step_size,
+  unknowns, 
+  min_h, max_h,
+  time_step_size,
   flux = True,
   ncp  = False,
   use_gpu =  False
@@ -88,10 +83,9 @@ project.set_global_simulation_parameters(
 project.set_load_balancing( "toolbox::loadbalancing::RecursiveSubdivision", "(" + str(args.load_balancing_quality) + ")" )
 
 peano4_project = project.generate_Peano4_project()
-peano4_project.constants.export( "MaxHOfVolume", args.h )
 peano4_project.output.makefile.parse_configure_script_outcome( "../../../.." )
-peano4_project.output.makefile.add_library( project.get_core_library(build_mode), "../../../../src/exahype2" )
-peano4_project.output.makefile.add_library( "ToolboxLoadBalancing" + project.get_library_postfix(build_mode), "../../../../src/toolbox/loadbalancing" )
+#peano4_project.output.makefile.add_library( project.get_core_library(build_mode), "../../../../src/exahype2" )
+#peano4_project.output.makefile.add_library( "ToolboxLoadBalancing" + project.get_library_postfix(build_mode), "../../../../src/toolbox/loadbalancing" )
 peano4_project.output.makefile.set_mode(build_mode)
 peano4_project.build(make_clean_first=True,number_of_parallel_builds=12)
 
