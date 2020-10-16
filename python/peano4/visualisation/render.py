1d6705ab1fd6a0d3ceca8304e4fec7b2dd441cc4
Holger Schulz <hschulz@fnal.gov>
2020-10-16 10:19:17 +0100

    Add main and some protection
    Former-commit-id: 8f8767b4a312460b7a0642be4b8c9126a90b4b97

--- python/peano4/visualisation/render.py
+++ python/peano4/visualisation/render.py
@@ -201,6 +201,16 @@ def renderFile(filename):
   return
 
 
-filename = "./example_output/solution-Euler-4.peano-patch-file"
-renderFile(filename)
+if __name__ == "__main__":
+    import sys, os
+    if len(sys.argv) != 2:
+        print("Error, no input file given, exiting...")
+    sys.exit(1)
+    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        print("Error, specified input file '{}' does not exist, exiting...". format(filename))
+        sys.exit(1)
+    print("Rendering {}".format(filename))
+    renderFile(filename)
 
+    sys.exit(0)
