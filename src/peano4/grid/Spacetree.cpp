512f028763d248b5fb86f977aebe2c77305e0177
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2020-11-23 10:12:34 +0000

    Change info into debug message
    Former-commit-id: 9df5c1ad6f559cbabc37ec3a93460b2a4e9b179c

--- src/peano4/grid/Spacetree.cpp
+++ src/peano4/grid/Spacetree.cpp
@@ -2026,7 +2026,8 @@ void peano4::grid::Spacetree::receiveAndMergeUserData(
           );
 
           peano4::datamanagement::FaceMarker marker(enterCellTraversalEvent,inFacePositionWithinCell /*oppositeFace*/);
-          logDebug(
+          // @todo Debug
+          logInfo(
             "receiveAndMergeUserData(...)",
             "will merge face from periodic BC stack #" << fromStack << " with local face. Cell marker=" << marker.toString() <<
             ". in-face=" << inFacePositionWithinCell << ", opposite face=" << oppositeFace
@@ -2186,7 +2187,8 @@ void peano4::grid::Spacetree::sendUserData(const AutomatonState& state, Traversa
           peano4::datamanagement::FaceMarker marker(leaveCellTraversalEvent,outFacePositionWithinCell);
           const int toStack   = peano4::parallel::Node::getOutputStackForPeriodicBoundaryExchange(outFacePositionWithinCell);
 
-          logDebug(
+          // @todo Debug
+          logInfo(
             "sendUserData(...)",
             "send local face from stack " << outFaceStack << " of tree " << _id <<
             " to periodic BC stack #" << toStack << ". marker=" << marker.toString()
