bb82faed4e437836121f5609ae640e96ef48a149
Holger Schulz <holger.schulz@durham.ac.uk>
2020-12-14 18:25:00 +0000

    More stable and flexible script
    Former-commit-id: 37b6c43576e82d145c84c3e4520bbbdcb15a97f2

--- examples/exahype2/euler/example-scripts/finitevolumes-with-ExaHyPE2-CI.py
+++ examples/exahype2/euler/example-scripts/finitevolumes-with-ExaHyPE2-CI.py
@@ -23,11 +23,18 @@ import argparse
 parser = argparse.ArgumentParser(description='ExaHyPE 2 - Euler benchmarking script')
 parser.add_argument("--h",              dest="h",              type=float, help="Mesh size",  default=0.01 )
 parser.add_argument("--d",              dest="dim",            type=int,   help="Dimensions", default=2 )
+parser.add_argument("--s",              dest="patchsize",      type=int,   help="Patch size", default=17 )
 parser.add_argument("--p",              dest="plot",           type=bool,  help="Plot final solution", default=False )
 parser.add_argument("--n",              dest="number_of_time_steps",  type=int,  help="Number of timesteps", default=10 )
 parser.add_argument("--gpu",            dest="gpu",    action="store_true",      help="Use GPU", default=False )
+parser.add_argument("--m",               dest="mode",                     default="release", help="release|trace|asserts" )
 args = parser.parse_args()
 
+if args.mode not in ["release", "trace", "asserts"]:
+    print("Error, mode must be release, trace or assert, you supplied {}".format(args.mode))
+    parser.print_help()
+    import sys
+    sys.exit(1)
 
 #
 # Create a project and configure it to end up in a subnamespace (and thus
@@ -39,7 +46,7 @@ project = exahype2.Project( ["examples", "exahype2", "euler"], "finitevolumes", 
 #
 # Add the Finite Volumes solver
 #
-patch_size     = 10
+patch_size     = args.patchsize
 unknowns       = 5
 time_step_size = 0.000001
 min_h          = args.h
@@ -76,11 +83,12 @@ project.add_solver( solver );
 
 
 dimensions = args.dim
-build_mode = peano4.output.CompileMode.Release
-#build_mode = peano4.output.CompileMode.Trace
-#build_mode = peano4.output.CompileMode.Asserts
-
-
+if (args.mode=="release"):
+  build_mode = peano4.output.CompileMode.Release
+elif (args.mode=="trace"):
+  build_mode = peano4.output.CompileMode.Trace
+else:
+  build_mode = peano4.output.CompileMode.Asserts
 
 #
 # Lets configure some global parameters
@@ -105,7 +113,10 @@ project.set_Peano4_installation("../../..", build_mode)
 peano4_project = project.generate_Peano4_project()
 peano4_project.output.makefile.parse_configure_script_outcome( "../../.." )
 if args.gpu:
-  peano4_project.output.makefile.add_gpu_object( "../../../src/exahype2/fv/libExaHyPE2Core2d_a-Generic.o" )
+  if args.mode != "release":
+    peano4_project.output.makefile.add_gpu_object( "../../../src/exahype2/fv/libExaHyPE2Core2d_{}_a-Generic.o".format(args.mode) )
+  else:
+    peano4_project.output.makefile.add_gpu_object( "../../../src/exahype2/fv/libExaHyPE2Core2d_a-Generic.o" )
 
 peano4_project.generate()
 #peano4_project.build(make_clean_first=True, number_of_parallel_builds=1)
