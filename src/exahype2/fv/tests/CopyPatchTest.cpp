4bcd49f62e8801df3e666f8edd68a0b195b2b350
Holger Schulz <holger.schulz@durham.ac.uk>
2020-12-14 13:56:19 +0000

    Replication of unit test structure found in tarch for exahype2, add a first dummy test case --- this code is working from a technical point of view --- there is no real test coded yet
    Former-commit-id: 4085607b5517cdd1edfddfa5b833b11cf1c6744b

---
+++ src/exahype2/fv/tests/CopyPatchTest.cpp
@@ -0,0 +1,21 @@
+// This file is part of the Peano project. For conditions of distribution and
+// use, please see the copyright notice at www.peano-framework.org
+#include "CopyPatchTest.h"
+//#include "tarch/la/LUDecomposition.h"
+//#include "tarch/la/Matrix.h"
+//#include "tarch/la/Vector.h"
+
+
+
+exahype2::fv::tests::CopyPatchTest::CopyPatchTest():
+  TestCase ("exahype2::fv::CopyPatchTest") {
+}
+
+
+void exahype2::fv::tests::CopyPatchTest::run() {
+  testMethod (testMe);
+}
+
+void exahype2::fv::tests::CopyPatchTest::testMe() {
+   /// Add implementation of testcase here
+}
