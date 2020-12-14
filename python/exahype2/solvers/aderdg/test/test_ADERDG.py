3154b1b32e1ab922010c33d056e43a0de742b99d
Dominic Etienne Charrier <dominic.charrier@googlemail.com>
2020-12-14 17:30:37 +0100

    LagrangeBasis: Fix indent. aderdg/test: Create ADERDG object.
    Former-commit-id: 5c2e419299da5dd5e2fd26b90df93af7d7a178c9

--- python/exahype2/solvers/aderdg/test/test_ADERDG.py
+++ python/exahype2/solvers/aderdg/test/test_ADERDG.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-import exahype2.solvers.aderdg.ADERDG as aderdg
+from exahype2.solvers.aderdg.ADERDG import ADERDG, Polynomials
+
+aderdg = ADERDG(
+  name                 = "MyADERDG",
+  order                = 4,
+  unknowns             = 5,
+  auxiliary_variables  = 2,
+  polynomials          = Polynomials.Gauss_Legendre,
+  min_h                = 1e-2,
+  max_h                = 1e-1,
+  plot_grid_properties = None)
+
+print(aderdg)
