3626f59d89586989d493a4b85efefa12fccb605a
Holger Schulz <holger.schulz@durham.ac.uk>
2020-12-17 18:11:58 +0000

    not really a test but printing all the indices for now
    Former-commit-id: 1407eac10d1565abe2159c6cd2e4a299eeec0c7d

--- src/exahype2/fv/tests/CopyPatchTest.cpp
+++ src/exahype2/fv/tests/CopyPatchTest.cpp
@@ -1,3 +1,4 @@
+#include "peano4/utils/Loop.h"
 
 // This file is part of the Peano project. For conditions of distribution and
 // use, please see the copyright notice at www.peano-framework.org
@@ -12,5 +13,35 @@ void exahype2::fv::tests::CopyPatchTest::run() {
 }
 
 void exahype2::fv::tests::CopyPatchTest::testMe() {
-   /// Add implementation of testcase here
+  int    unknowns(5);
+  int    auxiliaryVariables(2);
+  int    numberOfVolumesPerAxisInPatch(5);
+  int    haloSize(1);
+  double*  QinWithHalo;
+  double* QOutWithoutHalo;
+  dfor(k,numberOfVolumesPerAxisInPatch) {
+    tarch::la::Vector<Dimensions,int>   source = k + tarch::la::Vector<Dimensions,int>(haloSize);
+    int sourceSerialised      = peano4::utils::dLinearised(source,numberOfVolumesPerAxisInPatch+haloSize*2);
+    int destinationSerialised = peano4::utils::dLinearised(k,numberOfVolumesPerAxisInPatch);
+    std::cerr << k << " " <<  sourceSerialised << " " <<  destinationSerialised << " \n";
+    for (int i=0; i<unknowns+auxiliaryVariables; i++) {
+      //QOutWithoutHalo[destinationSerialised*(unknowns+auxiliaryVariables)+i] = QinWithHalo[sourceSerialised*(unknowns+auxiliaryVariables)+i];
+    }
+  }
+ 
+  std::cerr << "Now the alternative implementation without dfor\n"; 
+  int sourceSerialised      = numberOfVolumesPerAxisInPatch+haloSize*2+haloSize;
+  int destinationSerialised = 0;
+  for (int y=0; y<numberOfVolumesPerAxisInPatch; y++) {
+    for (int x=0; x<numberOfVolumesPerAxisInPatch; x++) {
+       std::cerr << "[" << x <<","<< y << "] "<< sourceSerialised << " " <<  destinationSerialised << "\n";
+      for (int i=0; i<unknowns+auxiliaryVariables; i++) {
+        //QOutWithoutHalo[destinationSerialised*(unknowns+auxiliaryVariables)+i] = QinWithHalo[sourceSerialised*(unknowns+auxiliaryVariables)+i];
+      }
+      sourceSerialised++;
+      destinationSerialised++;
+    }
+    sourceSerialised += 2*haloSize;
+  }
+
 }
