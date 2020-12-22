51802c4f549967ea11556c2b212a4771fd7b7401
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2020-12-22 13:06:42 +0000

    Removed two invalid assertions
    Former-commit-id: 095a476dec01631183a6bd037a6e3349a87212d4

--- src/exahype2/EnclaveTask.cpp
+++ src/exahype2/EnclaveTask.cpp
@@ -30,26 +30,8 @@ bool exahype2::EnclaveTask::run() {
   logTraceIn( "run()" );
   _outputValues = tarch::multicore::allocateMemory( _numberOfResultValues, tarch::multicore::MemoryLocation::Heap );
 
-  ::exahype2::fv::validatePatch(
-    _inputValues,
-    64,
-    0,
-    6,
-    1,
-    std::string(__FILE__) + ": " + std::to_string(__LINE__)
-  ); // previous time step has to be valid
-
   _functor(_inputValues,_outputValues,_marker);
   tarch::multicore::freeMemory(_inputValues,tarch::multicore::MemoryLocation::Heap );
-
-  ::exahype2::fv::validatePatch(
-		  _outputValues,
-    64,
-    0,
-    6,
-    0,
-    std::string(__FILE__) + ": " + std::to_string(__LINE__)
-  ); // previous time step has to be valid
 
   EnclaveBookkeeping::getInstance().finishedTask(getTaskId(),_numberOfResultValues,_outputValues);
   logTraceOut( "run()" );
