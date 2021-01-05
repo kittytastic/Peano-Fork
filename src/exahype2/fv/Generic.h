826fe1f74c53ec36668647bbc95338ec6c838d21
tobias <tobias@UNKNOWN>
2021-01-05 20:43:02 +0000

    Added docu
    Former-commit-id: 7382d8fd435e4f815faf5fdcadd064226a8057ae

--- src/exahype2/fv/Generic.h
+++ src/exahype2/fv/Generic.h
@@ -62,14 +62,33 @@ namespace exahype2 {
      * Is a face loop-based implementation of a split 1d Riemann.
      *
      * You are given two arrays: The reconstructed one and Qout. Qout has the
-     * dimensions numberOfVolumesPerAxisInPatch x numberOfVolumesPerAxisInPatch.
-     * The reconstructed patch Qin has the dimensions (numberOfVolumesPerAxisInPatch+1) x
-     * (numberOfVolumesPerAxisInPatch+1). The data within the patch is stored as
-     * AoS.
+     * dimensions numberOfVolumesPerAxisInPatch x 
+     * numberOfVolumesPerAxisInPatch. The reconstructed patch Qin has the 
+     * dimensions (numberOfVolumesPerAxisInPatch+1) x
+     * (numberOfVolumesPerAxisInPatch+1). The data within the patch is stored 
+     * as AoS.
+     * 
+     * The routine runs over all faces within the input array and computes how 
+     * much flows through these faces. It calls splitRiemannSolve1d() per face. 
+     * This routine tells what flows into the left and into the right adjacent 
+     * volume. This outflow is added to the respective volume in Qout. 
      *
-     * Please consult the guidebook for some documentation of this routine. Some
-     * background is given in its ExaHyPE chapter, but the really interesting
-     * details are discussed within the Finite Volume chapter.
+     * @image html applySplit1DRiemannToPatch_Overlap1AoS2d.png
+     *
+     * <h2> Implementation </h2>
+     * 
+     * There are two big blocks. The first block runs over all faces with a 
+     * normal along the x-direction. The second block over faces with normal 
+     * along y-direction. For a 3x3 mesh as in the sketch above, the input 
+     * patch has 5x5 volumes as we also get the halo. The output is only 3x3.
+     * Each block is a nested for loop where one index runs over 0-2 and the
+     * other one through four indices.
+     *
+     * We touch a face (see red one in sketch), e.g., and add the outcome to 
+     * the volume of the left and right neighbour. Obviously, special care is 
+     * required for the very left and very right face. They have a left and 
+     * right adjacent volume in the preimage which feeds into the Riemann 
+     * solve, but the image hosts only the left or right volume.
      */
     void applySplit1DRiemannToPatch_Overlap1AoS2d(
       std::function< void(
