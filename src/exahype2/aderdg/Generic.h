2c0b8501591b1cd136c4a4409844d821a2540d7c
Dominic Etienne Charrier <dominic.charrier@googlemail.com>
2021-01-07 20:40:23 +0100

    Added some docu.
    Former-commit-id: 457c3979947a8c831d1f2aca821abfae79e226ab

--- src/exahype2/aderdg/Generic.h
+++ src/exahype2/aderdg/Generic.h
@@ -25,9 +25,9 @@ namespace exahype2 {
      * Just runs over the patch and ensures that no entry is non or infinite.
      *
      * @param[in] UIn
-     * @param[in] unknowns
-     * @param[in] auxiliaryVariables
-     * @param[in] order
+     * @param[in] unknowns the number of PDE unknowns that we evolve
+     * @param[in] auxiliaryVariables other quantities such as material parameters that we do not evolve
+     * @param[in] order the DG approximation order, which corresponds to order+1 DG nodes/Lagrange basis functions per coordinate axis
      * @param[in] location String that tells system from where this routine got called
      */
     void validatePatch(
@@ -42,9 +42,9 @@ namespace exahype2 {
      * Just runs over the space-time patch and ensures that no entry is non or infinite.
      *
      * @param[in] QIn
-     * @param[in] unknowns
-     * @param[in] auxiliaryVariables
-     * @param[in] order
+     * @param[in] unknowns the number of PDE unknowns that we evolve
+     * @param[in] auxiliaryVariables other quantities such as material parameters that we do not evolve
+     * @param[in] order the DG approximation order, which corresponds to order+1 DG nodes/Lagrange basis functions per coordinate axis
      * @param[in] location String that tells system from where this routine got called
      */
     void validateSpacetimePatch(
