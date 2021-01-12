6ce0f7fc2e950f335ba3aa3017a37c68fed2f485
Holger Schulz <holger.schulz@durham.ac.uk>
2021-01-12 17:41:52 +0000

    cleanup test
    Former-commit-id: d5f258a7a2728383290d3909d79ef64e67fdc05e

--- src/exahype2/fv/tests/applySplit1DRiemannToPatch_Overlap1AoSTest.cpp
+++ src/exahype2/fv/tests/applySplit1DRiemannToPatch_Overlap1AoSTest.cpp
@@ -44,7 +44,12 @@ void exahype2::fv::tests::applySplit1DRiemannToPatch_Overlap1AoSTest::testMe() {
   double dt = 0.1;
 
   double * Qout= new double[500];
-  for (unsigned int i=0;i<500;++i) Qout[i]=0.0;
+  double * Qcmp= new double[500];
+  for (unsigned int i=0;i<500;++i) 
+  {
+     Qout[i]=0.0;
+     Qcmp[i]=1.0*i;
+  }
 
   for (int y=0; y<numberOfVolumesPerAxisInPatch; y++)
   {
@@ -53,19 +58,20 @@ void exahype2::fv::tests::applySplit1DRiemannToPatch_Overlap1AoSTest::testMe() {
       const int leftVoxelInImage     =  x - 1 + y * numberOfVolumesPerAxisInPatch;
       const int rightVoxelInImage    =  x     + y * numberOfVolumesPerAxisInPatch;
 
-      tarch::la::Vector<Dimensions,double> volumeX = patchCentre - 0.5 * patchSize;
-      volumeX(0) +=       x * volumeH(0);
-      volumeX(1) += (y+0.5) * volumeH(1);
 
       for (int unknown=0; unknown<unknowns; unknown++)
       {
         if (x>0)
         { 
            Qout[ leftVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] -= dt / volumeH(0) * numericalFluxL[unknown];
+           //printf("loopA,%d,%d,%d,%d,%d,%d\n", x, y, leftVoxelInImage, rightVoxelInImage, unknown, leftVoxelInImage*(unknowns+auxiliaryVariables)+unknown);
+           Qout[ leftVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ leftVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
         }
         if (x<numberOfVolumesPerAxisInPatch)
         {
            Qout[ rightVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += dt / volumeH(0) * numericalFluxR[unknown];
+           //printf("loopA,%d,%d,%d,%d,%d,%d\n", x, y, leftVoxelInImage, rightVoxelInImage, unknown, rightVoxelInImage*(unknowns+auxiliaryVariables)+unknown);
+           Qout[ rightVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ rightVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
         }
       }
     }
@@ -78,18 +84,19 @@ void exahype2::fv::tests::applySplit1DRiemannToPatch_Overlap1AoSTest::testMe() {
       const int lowerVoxelInImage    = x + (y-1) * numberOfVolumesPerAxisInPatch;
       const int upperVoxelInImage    = x   + y   * numberOfVolumesPerAxisInPatch;
 
-      tarch::la::Vector<Dimensions,double> volumeX = patchCentre - 0.5 * patchSize;
-      volumeX(0) += (x+0.5) * volumeH(0);
-      volumeX(1) +=       y * volumeH(1);
 
       for (int unknown=0; unknown<unknowns; unknown++) {
         if (y>0)
         {
           Qout[ lowerVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] -= dt / volumeH(0) * numericalFluxL[unknown];
+           //printf("loopB,%d,%d,%d,%d,%d,%d\n", x, y, upperVoxelInImage, lowerVoxelInImage, unknown, lowerVoxelInImage*(unknowns+auxiliaryVariables)+unknown);
+          Qout[ lowerVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ lowerVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
         }
         if (y<numberOfVolumesPerAxisInPatch)
         {
           Qout[ upperVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += dt / volumeH(0) * numericalFluxR[unknown];
+           //printf("loopB,%d,%d,%d,%d,%d,%d\n", x, y, upperVoxelInImage, lowerVoxelInImage, unknown, upperVoxelInImage*(unknowns+auxiliaryVariables)+unknown);
+          Qout[ upperVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ upperVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
         }
       }
     }
@@ -110,19 +117,20 @@ void exahype2::fv::tests::applySplit1DRiemannToPatch_Overlap1AoSTest::testMe() {
           const int leftVoxelInImage     = x-1 + y * numberOfVolumesPerAxisInPatch;
           const int rightVoxelInImage    = x   + y * numberOfVolumesPerAxisInPatch;
 
-          tarch::la::Vector<Dimensions,double> volumeX = patchCentre - 0.5 * patchSize;
-          volumeX(0) +=       x * volumeH(0);
-          volumeX(1) += (y+0.5) * volumeH(1);
 
           for (int unknown=0; unknown<unknowns; unknown++)
           {
              if (x>0)
              {
                 Qouttest[ leftVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] -= dt / volumeH(0) * numericalFluxL[unknown];
+              //printf("loopC,%d,%d,%d,%d,%d,%d\n", x, y, leftVoxelInImage, rightVoxelInImage, unknown, leftVoxelInImage*(unknowns+auxiliaryVariables)+unknown);
+                Qouttest[ leftVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ leftVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
              }
              if (x<numberOfVolumesPerAxisInPatch)
              {
                 Qouttest[ rightVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += dt / volumeH(0) * numericalFluxR[unknown];
+              //printf("loopC,%d,%d,%d,%d,%d,%d\n", x, y, leftVoxelInImage, rightVoxelInImage, unknown, rightVoxelInImage*(unknowns+auxiliaryVariables)+unknown);
+                Qouttest[ rightVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ rightVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
              }
           }
        }
@@ -138,31 +146,97 @@ void exahype2::fv::tests::applySplit1DRiemannToPatch_Overlap1AoSTest::testMe() {
            const int lowerVoxelInImage    = x + (y-1) * numberOfVolumesPerAxisInPatch;
            const int upperVoxelInImage    = x     + y * numberOfVolumesPerAxisInPatch;
 
-           tarch::la::Vector<Dimensions,double> volumeX = patchCentre - 0.5 * patchSize;
-           volumeX(0) += (x+0.5) * volumeH(0);
-           volumeX(1) +=       y * volumeH(1);
 
            for (int unknown=0; unknown<unknowns; unknown++)
            {
               if (y>0)
               {
                  Qouttest[ lowerVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] -= dt / volumeH(0) * numericalFluxL[unknown];
+           //printf("loopD,%d,%d,%d,%d,%d,%d\n", x, y, upperVoxelInImage, lowerVoxelInImage, unknown, lowerVoxelInImage*(unknowns+auxiliaryVariables)+unknown);
+                Qouttest[ lowerVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ lowerVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
               }
               if (y<numberOfVolumesPerAxisInPatch)
               {
                  Qouttest[ upperVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += dt / volumeH(0) * numericalFluxR[unknown];
+           //printf("loopD,%d,%d,%d,%d,%d,%d\n", x, y, upperVoxelInImage, lowerVoxelInImage, unknown, upperVoxelInImage*(unknowns+auxiliaryVariables)+unknown);
+                Qouttest[ upperVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ upperVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
               }
            }
         }
      }
   }
   
+  // Done with routine 2
+  double * Qouttest2= new double[500];
+  for (unsigned int i=0;i<500;++i) Qouttest2[i]=0.0;
+      for (int shift = 0; shift < 2; shift++)
+      {
+        #ifdef SharedOMP
+        #pragma omp parallel for collapse(2)
+        #endif
+        for (int x = shift; x <= numberOfVolumesPerAxisInPatch; x += 2)
+        {
+          for (int y = 0; y < numberOfVolumesPerAxisInPatch; y++)
+          {
+            const int leftVoxelInImage     = x - 1 + y * numberOfVolumesPerAxisInPatch;
+            const int rightVoxelInImage    = x     + y * numberOfVolumesPerAxisInPatch;
+
+            for (int unknown = 0; unknown < unknowns; unknown++)
+            {
+              if (x > 0)
+              {
+                Qouttest2[leftVoxelInImage * (unknowns + auxiliaryVariables) + unknown]  -= dt / volumeH (0) * numericalFluxL[unknown];
+                Qouttest2[ leftVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ leftVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
+              }
+              if (x < numberOfVolumesPerAxisInPatch)
+              {
+                Qouttest2[rightVoxelInImage * (unknowns + auxiliaryVariables) + unknown] += dt / volumeH (0) * numericalFluxR[unknown];
+                Qouttest2[ rightVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ rightVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
+              }
+            }
+          }
+        }
+      }
+
+       //Iterate over other normal
+      for (int shift = 0; shift < 2; shift++)
+      {
+        #ifdef SharedOMP
+        #pragma omp parallel for simd collapse(2)
+        #endif
+        for (int y = shift; y <= numberOfVolumesPerAxisInPatch; y += 2)
+        {
+          for (int x = 0; x < numberOfVolumesPerAxisInPatch; x++)
+          {
+            const int lowerVoxelInImage    = x      + (y - 1) *      numberOfVolumesPerAxisInPatch ;
+            const int upperVoxelInImage    = x      +       y *      numberOfVolumesPerAxisInPatch ;
+
+            for (int unknown = 0; unknown < unknowns; unknown++)
+            {
+              if (y > 0)
+              {
+                Qouttest2[lowerVoxelInImage * (unknowns + auxiliaryVariables) + unknown] -= dt / volumeH (0) * numericalFluxL[unknown];
+                Qouttest2[ lowerVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ lowerVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
+              }
+              if (y < numberOfVolumesPerAxisInPatch)
+              {
+                Qouttest2[upperVoxelInImage * (unknowns + auxiliaryVariables) + unknown] += dt / volumeH (0) * numericalFluxR[unknown];
+                Qouttest2[ upperVoxelInImage*(unknowns+auxiliaryVariables)+unknown ] += Qcmp[ upperVoxelInImage*(unknowns+auxiliaryVariables)+unknown ];
+              }
+            }
+          }
+        }
+      }
+
 
   // Elementwise comparison
 
   for (unsigned int i=0;i<500;++i)
   {
-     validateEquals(Qout[i], Qouttest[i]);
+     //printf("%d: %f vs %f\n", i, Qout[i], Qouttest[i]);
+     validateEquals(Qout[i],     Qouttest[i]);
+     //printf("%f vs %f\n", Qouttest[i], Qouttest2[i]);
+     validateEquals(Qouttest[i], Qouttest2[i]);
   }
 
 
