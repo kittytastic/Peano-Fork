b0afca7dd47cacdd2380eb2093755a029697a08a
tobias <tobias@UNKNOWN>
2021-01-13 16:41:19 +0000

    Start to rewrite the conversion scripts in Python
    Former-commit-id: f53bc859a6085b6d3f8c5fe3f2b1fc23ef5a982a

---
+++ python/exahype2/solvers/aderdg/AbstractSolverFixedTimeStepSize.template.cpp
@@ -0,0 +1,92 @@
+#include "{{CLASSNAME}}.h"
+#include "Constants.h"
+
+tarch::logging::Log   {{NAMESPACE | join("::")}}::{{CLASSNAME}}::{{CLASSNAME}}::_log( "{{NAMESPACE | join("::")}}::{{CLASSNAME}}::{{CLASSNAME}}" );
+
+std::bitset<Dimensions> {{NAMESPACE | join("::")}}::{{CLASSNAME}}::{{CLASSNAME}}::PeriodicBC = {{NAMESPACE | join("::")}}::PeriodicBC;
+
+
+double {{NAMESPACE | join("::")}}::{{CLASSNAME}}::getMinMeshSize() const {
+  return _minH;
+}
+
+
+double {{NAMESPACE | join("::")}}::{{CLASSNAME}}::getMaxMeshSize() const {
+  return _maxH;
+}
+
+
+double {{NAMESPACE | join("::")}}::{{CLASSNAME}}::getMinTimeStamp() const {
+  return _timeStamp;
+}
+
+
+double {{NAMESPACE | join("::")}}::{{CLASSNAME}}::getMaxTimeStamp() const {
+  return _timeStamp;
+}
+
+
+double {{NAMESPACE | join("::")}}::{{CLASSNAME}}::getMinTimeStepSize() const {
+  return {{TIME_STEP_SIZE}};
+}
+
+
+double {{NAMESPACE | join("::")}}::{{CLASSNAME}}::getMaxTimeStepSize() const {
+  return {{TIME_STEP_SIZE}};
+}
+
+
+{{NAMESPACE | join("::")}}::{{CLASSNAME}}::SolverState {{NAMESPACE | join("::")}}::{{CLASSNAME}}::getSolverState() const {
+  return _solverState;
+}
+
+
+
+{% if REFINEMENT_CRITERION_IMPLEMENTATION!="<user-defined>" %}
+::exahype2::RefinementCommand {{NAMESPACE | join("::")}}::{{CLASSNAME}}::refinementCriterion(
+  const double * __restrict__                 Q, // Q[{{NUMBER_OF_UNKNOWNS}}+{{NUMBER_OF_AUXILIARY_VARIABLES}}],
+  const tarch::la::Vector<Dimensions,double>& x,
+  const tarch::la::Vector<Dimensions,double>& cellH,
+  double                                      t
+) {
+  {% if REFINEMENT_CRITERION_IMPLEMENTATION=="<empty>" %}
+  ::exahype2::RefinementCommand result = ::exahype2::RefinementCommand::Keep;
+
+  if ( tarch::la::smallerEquals(_maxH,tarch::la::max(cellH)) ) {
+    result = ::exahype2::RefinementCommand::Refine;
+  }
+
+  return result;
+  {% else %}
+  {{REFINEMENT_CRITERION_IMPLEMENTATION}}
+  {% endif %}
+}
+{% endif %}
+
+
+{% if INITIAL_CONDITIONS_IMPLEMENTATION!="<user-defined>" %}
+void {{NAMESPACE | join("::")}}::{{CLASSNAME}}::adjustSolution(
+  double * __restrict__                       Q,
+  const tarch::la::Vector<Dimensions,double>& x,
+  double                                      t
+) {
+  if (tarch::la::equals(t,0.0) ) {
+    {{INITIAL_CONDITIONS_IMPLEMENTATION}}
+  }
+}
+{% endif %}
+
+
+
+{% if BOUNDARY_CONDITIONS_IMPLEMENTATION!="<user-defined>" %}
+void {{NAMESPACE | join("::")}}::{{CLASSNAME}}::boundaryConditions(
+  const double * __restrict__                       Qinside, // Qinside[{{NUMBER_OF_UNKNOWNS}}+{{NUMBER_OF_AUXILIARY_VARIABLES}}]
+  double * __restrict__                       Qoutside // Qoutside[{{NUMBER_OF_UNKNOWNS}}+{{NUMBER_OF_AUXILIARY_VARIABLES}}]
+  const tarch::la::Vector<Dimensions,double>& x,
+  double                                      t,
+  int                                         normal
+) {
+  {{BOUNDARY_CONDITIONS_IMPLEMENTATION}}
+}
+{% endif %}
+
