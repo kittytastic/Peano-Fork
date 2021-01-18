b941addbab9fb88390e115b440b17ea2474a46c1
Dominic Etienne Charrier <dominic.charrier@googlemail.com>
2021-01-18 23:18:23 +0100

    Sketch a test case using input data from ExaHyPE1.
    Former-commit-id: 522ac5e2813fce3dd78cbd1e1dd7a30b0e615ea9

---
+++ src/exahype2/aderdg/tests/ADERDGTest.h
@@ -0,0 +1,77 @@
+#if 0
+
+// This file is part of the Peano project. For conditions of distribution and
+// use, please see the copyright notice at www.peano-framework.org
+#ifndef _EXAHYPE2_ADERDG_TESTS_ADERDGTEST_H_
+#define _EXAHYPE2_ADERDG_TESTS_ADERDGTEST_H_
+
+#include "tarch/tests/TestCase.h"
+
+namespace exahype2 {
+  namespace aderdg {
+    namespace tests {
+      class ADEiRDGTest;
+    }
+  }
+}
+
+/**
+ * Provides tests for types Vector, DynamicVector and all Vector functionality.
+ */
+class exahype2::aderdg::tests::ADERDGTest : public tarch::tests::TestCase
+{
+private:
+
+  /**
+   * Operators
+   */
+  const double QuadraturePoints[3+1];
+  const double QuadratureWeights[3+1];
+  const double BasisFunctionValuesLeft[3+1];
+  const double BasisFunctionValuesRight[3+1];
+  const double DerivativeOperator[(3+1)*(3+1)];
+  const double MassMatrix[(3+1)*(3+1)];
+  const double StiffnessOperator[(3+1)*(3+1)];
+  const double InvertedPredictorLhsOperator[(3+1)*(3+1)];
+  const double EquidistantGridProjector[(3+1)*(3+1)];
+  const double FineGridProjector[3][(3+1)*(3+1)];
+
+  /**
+   * Test input/output data from ExaHyPE1.
+   */
+  const double spaceTimePredictor_PicardLoop_UIn[80];
+  const double spaceTimePredictor_PicardLoop_QOut[320];
+
+  /**
+   * Tests ADER-DG Picard loop.
+   */
+  void test_spaceTimePredictor_PicardLoop_loop();
+
+public:
+
+  /**
+   * Cosntructor.
+   */
+  ADERDGTest ();
+
+  /**
+   * Destructor, empty.
+   */
+  virtual ~ADERDGTest () {};
+
+  /**
+   * This routine is triggered by the TestCaseCollection
+   */
+  virtual void run();
+
+  /**
+   * Setup your test case.
+   */
+  virtual void setUp() {};
+};
+
+
+
+#endif /* _EXAHYPE2_ADERDG_TESTS_ADERDGTEST_H_ */
+
+#endif
