7005e3a48284d5036b0cd6ec49961155933f9f92
Holger Schulz <holger.schulz@durham.ac.uk>
2021-02-02 09:17:30 +0000

    Overload Fusanov_2D with function that takes vector of patches
    Former-commit-id: 76a63cfb265d9e4d38c34c596c01cb94488e041e

--- src/exahype2/fv/Rusanov.h
+++ src/exahype2/fv/Rusanov.h
@@ -17,6 +17,241 @@
 namespace exahype2 {
   namespace fv {
 
+    template<
+      int                                          numVPAIP, // numberofVolumesPerAxisInPatch
+      int                                          unknowns,
+      int                                          auxiliaryVariables,
+      typename SOLVER>
+    void Fusanov_2D(
+      int                                          haloSize,
+      double                                       dt,
+      std::vector<std::tuple<double*, const double, int, double, double, double, double> > patchVec,
+      double * __restrict__                        Qout,
+      int                                          sourcePatchSize,
+      int                                          destPatchSize
+    )
+    {
+      const size_t NPT  = patchVec.size();
+      const size_t LTOT = NPT*destPatchSize;
+      const size_t LR   = NPT*sourcePatchSize;
+      double destinationPatch[LTOT];
+      //double * reconstructedPatch;//[sourcePatchSize];
+
+      double* RP = ::tarch::allocateMemory(sourcePatchSize*NPT, ::tarch::MemoryLocation::Heap);
+
+      // Move all data to the device, prepare arrays first
+      double T[NPT], X0[NPT], X1[NPT], H0[NPT], H1[NPT];
+      int TID[NPT];
+      for (size_t i=0; i<patchVec.size(); i++)
+      {
+        auto patch = patchVec[i];
+        T[i]    = std::get<1>(patch);
+        TID[i]  = std::get<2>(patch);
+        X0[i]   = std::get<3>(patch);
+        H0[i]   = std::get<4>(patch);
+        X1[i]   = std::get<5>(patch);
+        H1[i]   = std::get<6>(patch);
+        std::copy(std::get<0>(patch), std::get<0>(patch)+sourcePatchSize, RP + i*sourcePatchSize);
+      }
+
+ //map(alloc:destinationPatch[0:LTOT]) 
+#pragma omp target map(to:T[0:NPT]) map(to:TID[0:NPT]) map(to:X0[0:NPT]) map(to:X1[0:NPT])  map(to:H0[0:NPT]) map(to:H1[0:NPT]) map(to:RP[0:LR]) map(tofrom:destinationPatch[0:LTOT])//map(alloc:reconstructedPatch[0:sourcePatchSize])
+#pragma omp teams
+#pragma omp distribute 
+      for (size_t pidx=0;pidx<NPT;pidx++)
+      {
+        //auto patch = patchVec[pidx];
+        //double *reconstructedPatch = std::get<0>(patch);
+        double                    t =   T[pidx];
+        int                  taskId = TID[pidx];
+        double                   x0 =  X0[pidx];
+        double                   h0 =  H0[pidx];
+        double                   x1 =  X1[pidx];
+        double                   h1 =  H1[pidx];
+        double *reconstructedPatch = RP + LR*pidx;
+        //printf("x1*: %f\n", x1);
+        //printf("there %lu\n",patchVec.size());
+
+        //#pragma omp target update to( reconstructedPatch[0:sourcePatchSize])
+
+
+      #ifdef SharedOMP
+        #if defined(OpenMPGPUOffloading)
+        //#pragma omp target map(to:reconstructedPatch[0:sourcePatchSize])
+        #endif
+      #pragma omp parallel for collapse(2)
+      #endif
+        for (int x = 0; x < numVPAIP; x++)
+        {
+          for (int y = 0; y < numVPAIP; y++)
+          {
+
+            // This is copyPatch
+            for (int i=0; i<unknowns+auxiliaryVariables; i++)
+            {
+              int sourceIndex      = (y+1)*(numVPAIP+ 3*haloSize) + x - y;
+              int destinationIndex = y*numVPAIP + x;
+              destinationPatch[pidx*destPatchSize + destinationIndex*(unknowns+auxiliaryVariables)+i] = reconstructedPatch[sourceIndex*(unknowns+auxiliaryVariables)+i];
+            }
+            
+            tarch::la::Vector<2,double> patchCentre = {x0,x1};
+            tarch::la::Vector<2,double> patchSize   = {h0,h1};
+
+            tarch::la::Vector<2,double> volumeH = {patchSize(0)/numVPAIP,patchSize(1)/numVPAIP};
+            tarch::la::Vector<2, double> volumeX = {patchCentre(0)-0.5*patchSize(0), patchCentre(1)-0.5*patchSize(1)};
+            volumeX (0) += (x + 0.5) * volumeH (0);
+            volumeX (1) += (y + 0.5) * volumeH (1);
+            const int voxelInPreImage  = x+1      + (y+1) * (numVPAIP+2);
+            const int voxelInImage     = x            + y * numVPAIP;
+            double sourceTermContributions[unknowns];
+            SOLVER::sourceTerm( reconstructedPatch + voxelInPreImage * (unknowns + auxiliaryVariables),  volumeX, volumeH(0), t, dt, sourceTermContributions);
+
+            for (int unknown = 0; unknown < unknowns; unknown++)
+            {
+              destinationPatch[pidx*destPatchSize + voxelInImage * (unknowns + auxiliaryVariables) + unknown] += dt * sourceTermContributions[unknown];
+            }
+          }
+        }
+        
+        
+        for (int shift = 0; shift < 2; shift++)
+        {
+          #ifdef SharedOMP
+             //#if defined(OpenMPGPUOffloading)
+             //#pragma omp target
+             //#endif
+          #pragma omp parallel for collapse(2)
+          #endif
+
+          for (int x = shift; x <= numVPAIP; x += 2)
+          {
+            for (int y = 0; y < numVPAIP; y++)
+            {
+              tarch::la::Vector<2,double> patchCentre = {x0,x1};
+              tarch::la::Vector<2,double> patchSize   = {h0,h1};
+
+              const int leftVoxelInPreimage  = x +      (y + 1) * (2 + numVPAIP);
+              const int rightVoxelInPreimage = x + 1  + (y + 1) * (2 + numVPAIP);
+
+              const int leftVoxelInImage     = x - 1 + y * numVPAIP;
+              const int rightVoxelInImage    = x     + y * numVPAIP;
+
+
+              // getVolumeSize
+              tarch::la::Vector<2,double> volumeH = {patchSize(0)/numVPAIP,patchSize(1)/numVPAIP};
+              // Assignment vectorA = vectorB - 0.5*vectorC
+              tarch::la::Vector<2, double> volumeX = {patchCentre(0)-0.5*patchSize(0), patchCentre(1)-0.5*patchSize(1)};
+
+              volumeX(0) += x * volumeH (0);
+              volumeX(1) += (y + 0.5) * volumeH (1);
+
+              auto QL = reconstructedPatch + leftVoxelInPreimage  * (unknowns + auxiliaryVariables);
+              auto QR = reconstructedPatch + rightVoxelInPreimage * (unknowns + auxiliaryVariables);
+              
+              auto dx = volumeH(0);
+              int normal = 0;
+
+              double fluxFL[unknowns];
+              double fluxFR[unknowns];
+
+              // The arguments don't make any sense
+              SOLVER::flux( QL, volumeX, dx, t, normal, fluxFL );
+              SOLVER::flux( QR, volumeX, dx, t, normal, fluxFR );
+
+              double lambdaMaxL = SOLVER::maxEigenvalue(QL,volumeX,dx,t,normal);
+              double lambdaMaxR = SOLVER::maxEigenvalue(QR,volumeX,dx,t,normal);
+
+              double lambdaMax  = std::max( lambdaMaxL, lambdaMaxR );
+
+              for (int unknown = 0; unknown < unknowns; unknown++)
+              {
+                if (x > 0)
+                {
+                  double fl = 0.5 * fluxFL[unknown] + 0.5 * fluxFR[unknown] - 0.5 * lambdaMax * (QR[unknown] - QL[unknown]);
+                  destinationPatch[pidx*destPatchSize + leftVoxelInImage * (unknowns + auxiliaryVariables) + unknown]  -= dt / volumeH (0) * fl;
+                }
+                if (x < numVPAIP)
+                {
+                  double fr = 0.5 * fluxFL[unknown] + 0.5 * fluxFR[unknown] - 0.5 * lambdaMax * (QR[unknown] - QL[unknown]);
+                  destinationPatch[pidx*destPatchSize + rightVoxelInImage * (unknowns + auxiliaryVariables) + unknown] += dt / volumeH (0) * fr;
+                }
+              }
+            }
+          }
+        }
+
+         //Iterate over other normal
+        for (int shift = 0; shift < 2; shift++)
+        {
+          #ifdef SharedOMP
+             //#if defined(OpenMPGPUOffloading)
+             //#pragma omp target
+             //#endif
+          #pragma omp parallel for collapse(2)
+          #endif
+          for (int y = shift; y <= numVPAIP; y += 2)
+          {
+            for (int x = 0; x < numVPAIP; x++)
+            {
+              tarch::la::Vector<2,double> patchCentre = {x0,x1};
+              tarch::la::Vector<2,double> patchSize   = {h0,h1};
+              const int lowerVoxelInPreimage = x + 1  +       y * (2 + numVPAIP);
+              const int upperVoxelInPreimage = x + 1  + (y + 1) * (2 + numVPAIP);
+              const int lowerVoxelInImage    = x      + (y - 1) *      numVPAIP ;
+              const int upperVoxelInImage    = x      +       y *      numVPAIP ;
+
+              // getVolumeSize
+              tarch::la::Vector<2,double> volumeH = {patchSize(0)/numVPAIP,patchSize(1)/numVPAIP};
+              // Assignment vectorA = vectorB - 0.5*vectorC
+              tarch::la::Vector<2, double> volumeX = {patchCentre(0)-0.5*patchSize(0), patchCentre(1)-0.5*patchSize(1)};
+
+              volumeX (0) += (x + 0.5) * volumeH (0);
+              volumeX (1) +=         y * volumeH (1);
+
+              auto QL = reconstructedPatch + lowerVoxelInPreimage  * (unknowns + auxiliaryVariables);
+              auto QR = reconstructedPatch + upperVoxelInPreimage * (unknowns + auxiliaryVariables);
+              
+              auto dx = volumeH(0);
+              int normal = 1;
+
+              double fluxFL[unknowns];
+              double fluxFR[unknowns];
+
+              SOLVER::flux( QL, volumeX, dx, t, normal, fluxFL );
+              SOLVER::flux( QR, volumeX, dx, t, normal, fluxFR );
+
+              double lambdaMaxL = SOLVER::maxEigenvalue(QL,volumeX,dx,t,normal);
+              double lambdaMaxR = SOLVER::maxEigenvalue(QR,volumeX,dx,t,normal);
+              double lambdaMax  = std::max( lambdaMaxL, lambdaMaxR );
+
+              for (int unknown = 0; unknown < unknowns; unknown++)
+              {
+                if (y > 0)
+                {
+                  double fl = 0.5 * fluxFL[unknown] + 0.5 * fluxFR[unknown] - 0.5 * lambdaMax * (QR[unknown] - QL[unknown]);
+                  destinationPatch[pidx*destPatchSize + lowerVoxelInImage * (unknowns + auxiliaryVariables) + unknown] -= dt / volumeH (0) * fl;
+                }
+                if (y < numVPAIP)
+                {
+                  double fr = 0.5 * fluxFL[unknown] + 0.5 * fluxFR[unknown] - 0.5 * lambdaMax * (QR[unknown] - QL[unknown]);
+                  destinationPatch[pidx*destPatchSize + upperVoxelInImage * (unknowns + auxiliaryVariables) + unknown] += dt / volumeH (0) * fr;
+                }
+              }
+            }
+          }
+        }
+      }
+
+      ::tarch::freeMemory(RP, tarch::MemoryLocation::Heap);
+    };
+
+
+
+
+
+
+
+
     //#if defined(OpenMPGPUOffloading)
     //#pragma omp declare target
     //#endif
