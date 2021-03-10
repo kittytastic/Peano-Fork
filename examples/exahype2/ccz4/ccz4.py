8214d70afb9075e70b47d294bd0b7a5086aab1e7
tobias <tobias@UNKNOWN>
2021-03-10 17:04:33 +0000

    Started to modularise
    Former-commit-id: 29a41957c15a9f260f841c31666aab6af2461923

--- examples/exahype2/ccz4/ccz4.py
+++ examples/exahype2/ccz4/ccz4.py
@@ -1,50 +1,13 @@
 import os
+import argparse
+
 import peano4
 import exahype2
-
-
 import exahype2.sympy
-from _ast import Or
-
-
-
-"""
- Use these if and only if you wanna debug
-"""
-#SuperClass = exahype2.solvers.fv.GenericRusanovFixedTimeStepSize
-#SuperClass = exahype2.solvers.fv.GenericRusanovFixedTimeStepSizeWithEnclaves
-
-
-"""
- This is my simplest version of an adaptive time stepping scheme. This one 
- is extensively tested, i.e. use this one for experiments.
-"""
-#SuperClass = exahype2.solvers.fv.GenericRusanovAdaptiveTimeStepSize
-
-
-"""
- This is a more sophisticated version which should yield way better performance
- on nodes with many cores.
-"""
-#SuperClass = exahype2.solvers.fv.GenericRusanovAdaptiveTimeStepSizeWithEnclaves
-
-
-"""
- This is my most advanced solver. It should basically yield the same output as
- GenericRusanovAdaptiveTimeStepSizeWithEnclaves for CCZ4 with the Gauge wave
- however.
-"""
-SuperClass = exahype2.solvers.fv.GenericRusanovOptimisticTimeStepSizeWithEnclaves
-
-"""
-Here is the aderdg test.
-"""
-#SuperClass = exahype2.solvers.aderdg.NonFusedGenericRusanovFixedTimeStepSize
-
 
 
 class CCZ4Solver( SuperClass ):
-  def __init__(self, name, patch_size, min_h, max_h):
+  def __init__(self, name, patch_size, min_h, max_h ):
     unknowns = {
       "G":6,
       "K":6,
@@ -224,53 +187,70 @@ class CCZ4Solver( SuperClass ):
     self.create_data_structures()
     self.create_action_sets()
 
+
+modes = { 
+  "release": peano4.output.CompileMode.Release,
+  "trace":   peano4.output.CompileMode.Trace,
+  "assert":  peano4.output.CompileMode.Asserts,
+  "debug":   peano4.output.CompileMode.Debug,
+}
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='ExaHyPE 2 - CCZ4 script')
+    parser.add_argument("-cs",   "--cell-size",       dest="max_h",           type=float, default=0.4,  help="Mesh size" )
+    parser.add_argument("-ps",   "--patch-size",      dest="patch_size",      type=float, default=6,    help="Patch size, i.e. number of volumes per cell per direction" )
+    parser.add_argument("-plt",  "--plot-step-size",  dest="plot_step_size",  type=float, default=0.01, help="Plot step size (0 to switch it off)" )
+    parser.add_argument("-m",    "--mode",            dest="mode",            default="release",  help="|".join(modes.keys()) )
+    parser.add_argument("-ext",  "--extension",       dest="extension",       choices=["none", "gradient", "adm"],   default="none",  help="Pick extension, i.e. what should be plotted on top. Default is none" )
+    parser.add_argument("-impl", "--implementation",  dest="implementation",  choices=["fv-fixed", "fv-fixed-enclave", "fv-adaptive" ,"fv-adaptive-enclave", "fv-adaptive-optimistic"],   default="fv-adaptive-enclave",  help="Pick extension, i.e. what should be plotted on top. Default is none" )
+    args = parser.parse_args()
+
+    #global SuperClass
+    
+    if args.implementation=="fv-fixed":
+       SuperClass = exahype2.solvers.fv.GenericRusanovFixedTimeStepSize
+    if args.implementation=="fv-fixed-enclave":
+       SuperClass = exahype2.solvers.fv.GenericRusanovFixedTimeStepSizeWithEnclaves
+    if args.implementation=="fv-adaptive":
+       SuperClass = exahype2.solvers.fv.GenericRusanovAdaptiveTimeStepSize
+    if args.implementation=="fv-adaptive-enclave":
+       SuperClass = exahype2.solvers.fv.GenericRusanovAdaptiveTimeStepSizeWithEnclaves
+    if args.implementation=="fv-adaptive-optimistic":
+       SuperClass = exahype2.solvers.fv.GenericRusanovOptimisticTimeStepSizeWithEnclaves
+
+    """
+    Here is the aderdg test.
+    """
+    # SuperClass = exahype2.solvers.aderdg.NonFusedGenericRusanovFixedTimeStepSize
+
+    
     project = exahype2.Project( ["examples", "exahype2", "ccz4"], "ccz4" )
 
-    time_step_size      = 0.001
-    max_h               = 0.4
-    #max_h               = 0.2
-    min_h               = max_h
-    patch_size          = 6
-    order               = 3
-    unknowns            = 59
-    
+   
     if SuperClass==exahype2.solvers.aderdg.NonFusedGenericRusanovFixedTimeStepSize:
       my_solver = exahype2.solvers.aderdg.NonFusedGenericRusanovFixedTimeStepSize(
         "CCZ4", order, unknowns, 0, #auxiliary_variables
         exahype2.solvers.aderdg.Polynomials.Gauss_Legendre, 
         min_h, max_h, time_step_size,
-	flux = None,
-	ncp  = exahype2.solvers.aderdg.PDETerms.User_Defined_Implementation,
-	#source_term=exahype2.solvers.aderdg.PDETerms.User_Defined_Implementation
+        flux = None,
+        ncp  = exahype2.solvers.aderdg.PDETerms.User_Defined_Implementation
       )
-
-      #my_solver._solver_template_file_class_name = SuperClass.__name__
-
-      #pde = exahype2.sympy.PDE(unknowns=unknowns,auxiliary_variables=0,dimensions=3)
-
-      #my_solver.set_implementation(
-       # boundary_conditions=exahype2.solvers.aderdg.PDETerms.User_Defined_Implementation,
-       # flux=exahype2.solvers.aderdg.PDETerms.None_Implementation,
-       # ncp=exahype2.solvers.aderdg.PDETerms.User_Defined_Implementation,
-        #source_term=exahype2.solvers.aderdg.PDETerms.User_Defined_Implementation
-      #) 
     else:
-      my_solver = CCZ4Solver("CCZ4", patch_size, min_h, max_h)
-
-      my_solver.add_constraint_verification()
-      #my_solver.add_derivative_calculation()
+      my_solver = CCZ4Solver("CCZ4", args.patch_size, args.max_h, args.max_h)
+      
+      if args.extension=="gradient":
+        my_solver.add_derivative_calculation()
+      if args.extension=="adm":
+        my_solver.add_constraint_verification()
 
    
     project.add_solver(my_solver)    
     
-    build_mode = peano4.output.CompileMode.Asserts
-    #build_mode = peano4.output.CompileMode.Release
+    build_mode = modes[args.mode]
     
     dimensions = 3
     end_time = 1.0
-    snapshots = time_step_size*40
-    #snapshots = 0
 
     periodic_boundary_conditions = [True,True,True]          # Periodic BC
     #periodic_boundary_conditions = [False,False,False]
@@ -279,7 +259,7 @@ if __name__ == "__main__":
       dimensions,               # dimensions
       [-0.5, -0.5, -0.5],  [1.0, 1.0, 1.0],
       end_time,                 # end time
-      0.0, snapshots,   # snapshots
+      0.0, args.plot_step_size,   # snapshots
       periodic_boundary_conditions
     )
     
@@ -322,6 +302,6 @@ if __name__ == "__main__":
 
     peano4_project.generate( throw_away_data_after_generation=False )
     
-    peano4_project.build( make_clean_first = False )
+    peano4_project.build( make_clean_first = True )
     #!make -j4
 
