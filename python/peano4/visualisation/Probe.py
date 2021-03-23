daa5889cc6ce065719409457d28d13316533d6b8
MarkLTurner <markturner289@gmail.com>
2021-03-23 17:01:12 +0000

    Remove natsort dependency, read from metafile
    Former-commit-id: 28d733650d567a187968fe8b6bc80ac1cc5e8232

--- python/peano4/visualisation/Probe.py
+++ python/peano4/visualisation/Probe.py
@@ -3,19 +3,19 @@ import csv
 import os
 import sys
 
-import natsort
-
 from peano4.visualisation.OutputFileParser import OutputFileParser
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Peano 4 - probe a specific '
                                                'point in a set of Peano patch files')
   parser.add_argument('--input-file',     dest='filepath',       help='File path to a Peano patch file')
-  parser.add_argument('--input-dir',      dest='directory_path', help='Directory file path that stores Peano patch files')
+  parser.add_argument('--meta-file',      dest='metafile',       help='The main file that links to a set of Peano patch files - these must be in the same directory')
+  #parser.add_argument('--input-dir',      dest='directory_path', help='Directory file path that stores Peano patch files')
   parser.add_argument('-x',               dest='x_axis',         help='coordinate: x-axis', required=True, type=float)
   parser.add_argument('-y',               dest='y_axis',         help='coordinate: y-axis', required=True, type=float)
   parser.add_argument('-z',               dest='z_axis',         help='coordinate: z-axis', type=float)
   parser.add_argument('--set-identifier', dest='set_identifier', help='Name of the set of unknowns we want to extract', default='')
+  parser.add_argument('--ofname',         dest='ofname',         help='Name of the output csv to which the probe writes its results', default='probe_output.csv')
   args = parser.parse_args()
 
   if args.filepath:
@@ -31,17 +31,23 @@ if __name__ == "__main__":
       if probe_result:  
         writer.writerow(probe_result)
 
-  if args.directory_path:
-    if not os.path.exists(args.directory_path):
-      print("Error, specified input directory '{}' does not exist, exiting...".format(args.directory_path))
+  if args.metafile:
+    if not os.path.exists(args.metafile):
+      print("Error, specified input file '{}' does not exist, exiting...".format(args.directory_path))
       sys.exit(1)
-
-    with open('probe_output.csv', 'w') as csv_file:
+    
+    patch_file_names = []
+    with open(args.metafile, 'r') as metafile:
+        for line in metafile.readlines():
+            words = line.split()
+            if 'include' in words:
+                patch_file_names.append(words[1].strip('"'))
+    
+    with open(args.ofname, 'w') as csv_file:
       writer = csv.writer(csv_file, delimiter=',')
-      for file in natsort.natsorted(os.listdir(args.directory_path)):
-        ofparser = OutputFileParser(os.path.join(args.directory_path, file), args.set_identifier, 0)
+      for file in patch_file_names:
+        ofparser = OutputFileParser(os.path.join(os.path.dirname(args.metafile), file), args.set_identifier, 0)
         probe_result = ofparser.probe(args.x_axis, args.y_axis, args.z_axis)
         if probe_result:
           writer.writerow(probe_result)
-
 
