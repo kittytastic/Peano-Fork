8090406da0b688defc0ee3e1749dcf093bfced51
fin_zhang <dggx86@durham.ac.uk>
2021-04-07 09:23:00 +0100

    comment testing stuff out
    Former-commit-id: 2d87b4b2f39f5f1a5561b2fbff3ae5e57669bc80

--- examples/exahype2/ccz4/ccz4.py
+++ examples/exahype2/ccz4/ccz4.py
@@ -341,7 +341,7 @@ if __name__ == "__main__":
 
     project.set_Peano4_installation("../../..", build_mode)
 
-    project.set_output_path( "/cosma6/data/dp004/dc-zhan3/exahype2/sbh-fv4" )
+    #project.set_output_path( "/cosma6/data/dp004/dc-zhan3/exahype2/sbh-fv4" )
     #probe_point = [0,0,0]
     #project.add_plot_filter( probe_point,[0.0,0.0,0.0],1 )
 
