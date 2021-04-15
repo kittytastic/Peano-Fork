45a568ba68502e6334ecf4c93c76e114bc4d8c82
fin_zhang <dggx86@durham.ac.uk>
2021-04-15 12:11:28 +0100

    testing bbhs
    Former-commit-id: e08f1864888975e52573cb9b2eb55fd07fd99239

--- examples/exahype2/ccz4/libtwopunctures/TP_bindding.h
+++ examples/exahype2/ccz4/libtwopunctures/TP_bindding.h
@@ -23,13 +23,13 @@ namespace TP_bindding {
 		tp->TP_epsilon=1e-6;}
 		
 		if (swi==1){
-		tp->par_b=5.0;
+		tp->par_b=7.0;
 		tp->center_offset[0]=0.0; tp->center_offset[1]=0.0; tp->center_offset[2]=0.0;
 		tp->target_M_plus=1.0;//adm mass
-		tp->par_P_plus[0]=0.0; tp->par_P_plus[1]=5.0; tp->par_P_plus[2]=0.0;//linear momentum
+		tp->par_P_plus[0]=0.0; tp->par_P_plus[1]=0.0; tp->par_P_plus[2]=0.0;//linear momentum
 		tp->par_S_plus[0]=0.0; tp->par_S_plus[1]=0.0; tp->par_S_plus[2]=0.0;//spin
 		tp->target_M_minus=1.0;//adm mass
-		tp->par_P_minus[0]=0.0; tp->par_P_minus[1]=-5.0; tp->par_P_minus[2]=0.0;//linear momentum
+		tp->par_P_minus[0]=0.0; tp->par_P_minus[1]=0.0; tp->par_P_minus[2]=0.0;//linear momentum
 		tp->par_S_minus[0]=0.0; tp->par_S_minus[1]=0.0; tp->par_S_minus[2]=0.0; //spin		
 		tp->grid_setup_method="evaluation"; //evaluation or Taylor expansion
 		tp->TP_epsilon=1e-6;}
