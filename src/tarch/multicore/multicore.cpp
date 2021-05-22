d763e99368356b13c19edb02d58c1767c24d3856
tobias <tobias@UNKNOWN>
2021-05-22 06:46:10 +0100

    Clean up
    Former-commit-id: 908e7d736b0acb3875bc96622d2c6d52d02e1a98

--- src/tarch/multicore/multicore.cpp
+++ src/tarch/multicore/multicore.cpp
@@ -1,5 +1,11 @@
 #include "multicore.h"
 
+
+#ifdef UseSmartMPI
+#include "smartmpi.h"
+#endif
+
+
 #ifndef SharedMemoryParallelisation
 
 double* tarch::allocateMemory(int size, MemoryLocation location) {
@@ -11,3 +17,17 @@ void tarch::freeMemory(double* data, MemoryLocation location) {
   delete[] data;
 }
 #endif
+
+
+void exahype2::initSmartMPI() {
+  #ifdef UseSmartMPI
+  smartmpi::init( tarch::mpi::Rank::getInstance().getCommunicator() );
+  #endif
+}
+
+
+void exahype2::shutdownSmartMPI() {
+  #ifdef UseSmartMPI
+  smartmpi::shutdown();
+  #endif
+}
