b937bc61d1920907796329056eaac1e6aa65ba7f
tobias <tobias@UNKNOWN>
2021-06-09 20:46:31 +0100

    Fixed the AMR
    Former-commit-id: 2a355d7a37200e0fd438792fbc1a984ab36e21f5

--- python/peano4/toolbox/blockstructured/ProjectFacesInAdaptiveMesh.py
+++ python/peano4/toolbox/blockstructured/ProjectFacesInAdaptiveMesh.py
@@ -8,26 +8,34 @@ class ProjectFacesInAdaptiveMesh(ActionSet):
   """
   This class assumes that you have an 2MxNxN patch on your faces. 
   
-  patch          Instance of peano4.datamodel.Patch
-  patch_overlap  Instance of peano4.datamodel.Patch. Consult remark above about how the
-                 dimensions of this overlap patch have to match. 
+  patch: peano4.datamodel.Patch
+  patch_overlap_interpolation:  peano4.datamodel.Patch. 
+    Consult remark above about how the dimensions of this overlap 
+    patch have to match. This overlap is used for the 
+    interpolation. It can be the same as for the restriction.
+  patch_overlap_restriction: peano4.datamodel.Patch. 
+                 
   """
   
   
-  def __init__(self, patch_overlap, guard="true", additional_includes="" ):
+  def __init__(self, patch_overlap_interpolation, patch_overlap_restriction, clear_overlap_in_touch_first_time=True, guard="true", additional_includes="" ):
     self.d = {}
-    if patch_overlap.dim[0] % 2 != 0:
-      print( "Error: Patch associated to face has to have even number of cells. Otherwise, it is not a symmetric overlap." )
-      assert( patch_overlap.dim[0] % 2 == 0 )
+    if patch_overlap_interpolation.dim[0] % 2 != 0:
+      raise Exception( "Error: Patch associated to face has to have even number of cells. Otherwise, it is not a symmetric overlap." )
+    if patch_overlap_interpolation.dim != patch_overlap_restriction.dim:
+      raise Exception( "Error: Patches associated to face have to have same dimensions for restriction and interpolation." )
       
-    self.d[ "UNKNOWNS" ]                  = str(patch_overlap.no_of_unknowns)
-    self.d[ "DOFS_PER_AXIS" ]             = str(patch_overlap.dim[1])
-    self.d[ "OVERLAP" ]                   = str(int(patch_overlap.dim[0]/2))
-    self.d[ "FINE_GRID_FACE_ACCESSOR" ]   = "fineGridFace"  + patch_overlap.name
-    self.d[ "COARSE_GRID_FACE_ACCESSOR" ] = "coarseGridFaces"  + patch_overlap.name
+    self.d[ "UNKNOWNS" ]                  = str(patch_overlap_interpolation.no_of_unknowns)
+    self.d[ "DOFS_PER_AXIS" ]             = str(patch_overlap_interpolation.dim[1])
+    self.d[ "OVERLAP" ]                   = str(int(patch_overlap_interpolation.dim[0]/2))
+    self.d[ "FINE_GRID_FACE_ACCESSOR_INTERPOLATION" ]   = "fineGridFace"  + patch_overlap_interpolation.name
+    self.d[ "COARSE_GRID_FACE_ACCESSOR_INTERPOLATION" ] = "coarseGridFaces"  + patch_overlap_interpolation.name
+    self.d[ "FINE_GRID_FACE_ACCESSOR_RESTRICTION" ]   = "fineGridFace"  + patch_overlap_restriction.name
+    self.d[ "COARSE_GRID_FACE_ACCESSOR_RESTRICTION" ] = "coarseGridFaces"  + patch_overlap_restriction.name
     self.d[ "GUARD" ]                     = guard
     
-    self.additional_includes = additional_includes
+    self._clear_overlap_in_touch_first_time = clear_overlap_in_touch_first_time    
+    self.additional_includes                = additional_includes
 
 
   def get_constructor_body(self):
@@ -50,55 +58,54 @@ class ProjectFacesInAdaptiveMesh(ActionSet):
     return False
 
 
+  __Template_TouchFaceFirstTime = """
+  if ( {GUARD} ) {{
+    logTraceIn( "touchFaceFirstTime(...)---ProjectFacesInAdaptiveMesh" );
+
+    ::toolbox::blockstructured::clearHaloLayerAoS(
+      marker,
+      {DOFS_PER_AXIS},
+      {OVERLAP},
+      {UNKNOWNS},
+      {FINE_GRID_FACE_ACCESSOR_RESTRICTION}.value
+    );
+
+    logTraceOut( "touchFaceFirstTime(...)---ProjectFacesInAdaptiveMesh" );
+  }}
+"""
+
+
   __Template_CreateHangingFace = """
   if ( {GUARD} ) {{
-    logTraceIn( "createHangingFace(...)" );
-
-    ::toolbox::blockstructured::clearHalfOfHaloLayerAoS(
-      marker,
-      {DOFS_PER_AXIS},
-      {OVERLAP},
-      {UNKNOWNS},
-      false,
-      {FINE_GRID_FACE_ACCESSOR}.value
-    );
-
-    ::toolbox::blockstructured::clearHalfOfHaloLayerAoS(
-      marker,
-      {DOFS_PER_AXIS},
-      {OVERLAP},
-      {UNKNOWNS},
-      true,
-      {COARSE_GRID_FACE_ACCESSOR}(marker.getSelectedFaceNumber()).value
-    );
+    logTraceIn( "createHangingFace(...)---ProjectFacesInAdaptiveMesh" );
 
     ::toolbox::blockstructured::interpolateOntoOuterHalfOfHaloLayer_AoS_piecewise_constant(
       marker,
       {DOFS_PER_AXIS},
       {OVERLAP},
       {UNKNOWNS},
-      {FINE_GRID_FACE_ACCESSOR}.value,
-      {COARSE_GRID_FACE_ACCESSOR}(marker.getSelectedFaceNumber()).value
+      {FINE_GRID_FACE_ACCESSOR_INTERPOLATION}.value,
+      {COARSE_GRID_FACE_ACCESSOR_INTERPOLATION}(marker.getSelectedFaceNumber()).value
     );
     
-    logTraceOut( "createHangingFace(...)" );
+    logTraceOut( "createHangingFace(...)---ProjectFacesInAdaptiveMesh" );
   }}
 """
 
   __Template_DestroyHangingFace = """
   if ( {GUARD} ) {{
-    logTraceIn( "destroyHangingFace(...)" );
+    logTraceInWith3Arguments( "destroyHangingFace(...)---ProjectFacesInAdaptiveMesh", "{FINE_GRID_FACE_ACCESSOR_RESTRICTION}", "{COARSE_GRID_FACE_ACCESSOR_RESTRICTION}", marker.getSelectedFaceNumber() );
 
     ::toolbox::blockstructured::restrictOntoOuterHalfOfHaloLayer_AoS_piecewise_constant(
       marker,
       {DOFS_PER_AXIS},
       {OVERLAP},
       {UNKNOWNS},
-      {FINE_GRID_FACE_ACCESSOR}.value,
-      {COARSE_GRID_FACE_ACCESSOR}(marker.getSelectedFaceNumber()).value
+      {FINE_GRID_FACE_ACCESSOR_RESTRICTION}.value,
+      {COARSE_GRID_FACE_ACCESSOR_RESTRICTION}(marker.getSelectedFaceNumber()).value
     );
 
-    logTraceOut( "destroyHangingFace(...)" );
+    logTraceOut( "destroyHangingFace(...)---ProjectFacesInAdaptiveMesh" );
   }}
 """
 
@@ -110,6 +117,9 @@ class ProjectFacesInAdaptiveMesh(ActionSet):
       pass 
     if operation_name==ActionSet.OPERATION_DESTROY_HANGING_FACE:
       result = self.__Template_DestroyHangingFace.format(**self.d)
+      pass 
+    if operation_name==ActionSet.OPERATION_TOUCH_FACE_FIRST_TIME and self._clear_overlap_in_touch_first_time:
+      result = self.__Template_TouchFaceFirstTime.format(**self.d)
       pass 
     return result
 
