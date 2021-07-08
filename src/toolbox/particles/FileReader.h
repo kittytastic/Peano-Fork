18121e017ae7db258f53d9d39074b75d7c375084
tobias <tobias@UNKNOWN>
2021-07-08 14:05:06 +0100

    I can now control whether a particle is removed from the central database or not
    Former-commit-id: b6fa179eaa80c5eb5d5baba16b1bbb1011acdc91

--- src/toolbox/particles/FileReader.h
+++ src/toolbox/particles/FileReader.h
@@ -37,9 +37,23 @@ class toolbox::particles::FileReader {
      */
     void readDatFile( const std::string& filename, const double scale );
 
+    /**
+     *
+     *
+     * <h2> Overlapping vs. non-overlapping domain decomposition </h2>
+     *
+     * In an SPMD environment, it is convenient to make each rank maintain its own input
+     * database. As a consequence, ranks have redundant particles and we will have to clean
+     * up eventually. To mirror this behaviour in the multithreaded context, we can remove
+     * particles whenever we assign them to an inner vertex, but we should also hand them
+     * out redundantly for boundary vertices.
+     *
+     * @param remove Remove a particle form the internal database if it is a match
+     */
     std::list< tarch::la::Vector<Dimensions,double> > getParticlesWithinVoxel(
       const tarch::la::Vector<Dimensions,double>&  x,
-      const tarch::la::Vector<Dimensions,double>&  h
+      const tarch::la::Vector<Dimensions,double>&  h,
+      bool remove
     );
 
     void clear();
