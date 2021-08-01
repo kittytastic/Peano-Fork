ea608581cc089eed686fa04d6338e8944fc205e6
tobias <tobias@UNKNOWN>
2021-08-01 14:04:05 +0100

    Introduced a few new assertions
    Former-commit-id: dc02748e392867ce23c67ecdf9aef248b75b0371

---
+++ src/tarch/la/DynamicMatrix.h
@@ -0,0 +1,15 @@
+// This file is part of the Peano project. For conditions of distribution and
+// use, please see the copyright notice at www.peano-framework.org
+#ifndef _TARCH_LA_MATRIX_H_
+#define _TARCH_LA_MATRIX_H_
+
+
+namespace tarch {
+  namespace la {
+    class RuntimeMatrix;
+  }
+}
+
+
+
+#endif
