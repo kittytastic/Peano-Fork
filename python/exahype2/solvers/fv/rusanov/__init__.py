021dfc3eddbd8c2708f8ca90e9a5992c6691c26d
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2021-09-03 15:05:27 +0100

    Continue from home
    Former-commit-id: 8d5db8976d704d9d90891195cb4161dcf5a77987

--- python/exahype2/solvers/fv/rusanov/__init__.py
+++ python/exahype2/solvers/fv/rusanov/__init__.py
@@ -4,4 +4,4 @@ from exahype2.solvers.fv.PDETerms  import PDETerms
 
 from .GlobalFixedTimeStep                     import GlobalFixedTimeStep
 from .GlobalAdaptiveTimeStep                  import GlobalAdaptiveTimeStep
-from .GlobalFixedTimeStepWithEnclaveTasking   import GlobalFixedTimeStepWithEnclaveTasking
+#from .GlobalFixedTimeStepWithEnclaveTasking   import GlobalFixedTimeStepWithEnclaveTasking
