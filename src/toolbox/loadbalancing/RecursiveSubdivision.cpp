f18c7a28521458f44699c92de88a0bcd5a04bafe
tobias <tobias@UNKNOWN>
2021-09-18 15:58:14 +0100

    It is better to decrement the no of threads by one
    Former-commit-id: b56194674d2010f5df6a5d8e1b15d0f61f62256d

--- src/toolbox/loadbalancing/RecursiveSubdivision.cpp
+++ src/toolbox/loadbalancing/RecursiveSubdivision.cpp
@@ -465,7 +465,7 @@ toolbox::loadbalancing::RecursiveSubdivision::StrategyStep toolbox::loadbalancin
   ) {
     if (
       peano4::parallel::SpacetreeSet::getInstance().getLocalSpacetrees().size() >
-      std::min( tarch::multicore::Core::getInstance().getNumberOfThreads(), _configuration->getMaxLocalTreesPerRank(Configuration::Phase::APosterioriBalancingPerRank))
+      std::min( tarch::multicore::Core::getInstance().getNumberOfThreads()-1, _configuration->getMaxLocalTreesPerRank(Configuration::Phase::APosterioriBalancingPerRank))
     ) {
       static bool wroteWarning = false;
       if (not wroteWarning) {
