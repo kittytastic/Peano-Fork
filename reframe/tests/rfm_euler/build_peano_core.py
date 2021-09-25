fe12e9d6bd26db621de35cfb63dafb0524bbfe1a
markturner <mark.turner@durham.ac.uk>
2021-09-25 16:55:04 +0100

    Make test a compile only test
    Former-commit-id: 7d44449241f6edc4ac2436351875207496ca949e

--- reframe/tests/rfm_euler/build_peano_core.py
+++ reframe/tests/rfm_euler/build_peano_core.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from git import Repo
 import touch
 
@@ -19,7 +20,7 @@ don't need to build the core for every test case
 @rfm.parameterized_test(*([git_rev]
     for git_rev in [GIT_REV]
 ))
-class Build_peano_core(rfm.RegressionTest):
+class Build_peano_core(rfm.CompileOnlyRegressionTest):
     def __init__(self, git_rev):
 
         # 4 ranks here means the domain decomposition fails
@@ -51,9 +52,11 @@ class Build_peano_core(rfm.RegressionTest):
                 'CXXFLAGS="-fopenmp -std=c++17"',
                 "--with-mpi=mpiicpc",
             ]
-        
-        # trivial executable (as this is a builder job):
-        self.executable = "sleep 0"
-        
+
+    @run_after('compile')
+    def remove_files_obstructing_copying_of_peano(self):
         # We have to delete the .git/ folder else ReFrame fails to copy Peano to the output dir
-        self.prerun_cmds = ["rm -rf Peano/.git"]
+        try:
+            shutil.rmtree(os.path.join(str(self.stagedir), 'Peano', '.git'))
+        except OSError as e:
+            print("Error: %s - %s." % (e.filename, e.strerror))
