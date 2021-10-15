d14c82d5668de33300c6ba128222c2284126dae7
tobias <tobias@UNKNOWN>
2021-10-15 07:10:46 +0100

    More docu
    Former-commit-id: ffd055fd9759d36738c2864f6c0cd29029043b0e

--- python/peano4/visualisation/ExtractFineGridFilter.py
+++ python/peano4/visualisation/ExtractFineGridFilter.py
@@ -36,26 +36,31 @@ class ExtractFineGridFilter( Filter ):
      with throwing away patches - otherwise, you quickly end
      up with vis where small pieces are missing. 
      
-     I therefore introduce a Tolerance of around 10 percent.
-     If in doubt, I keep stuff.
+     I therefore introduce an expansion factor. I throw away
+     patches if they overlap. So to be more pessimistic, the 
+     expansion has to be set negative. 
+     
+     a: peano4.visualisation.Patch
+     
+     b: peano4.visualisation.Patch
     
     """
     result = False
     
     if dimensions==3:
-      Tolerance = 0.1 * max(a.size[0],b.size[0],a.size[1],b.size[1],a.size[2],b.size[2])
-      result = a.offset[0] + a.size[0] > b.offset[0] + Tolerance and \
-               a.offset[1] + a.size[1] > b.offset[1] + Tolerance  and \
-               a.offset[2] + a.size[2] > b.offset[2] + Tolerance  and \
-               a.offset[0] + Tolerance < b.offset[0] + b.size[0] and \
-               a.offset[1] + Tolerance < b.offset[1] + b.size[1] and \
-               a.offset[2] + Tolerance < b.offset[2] + b.size[2]
+      Expansion = -0.1 * min(a.size[0],b.size[0],a.size[1],b.size[1],a.size[2],b.size[2])
+      result = a.offset[0] + a.size[0] + Expansion > b.offset[0] and \
+               a.offset[1] + a.size[1] + Expansion > b.offset[1] and \
+               a.offset[2] + a.size[2] + Expansion > b.offset[2] and \
+               a.offset[0] < b.offset[0] + b.size[0] + Expansion and \
+               a.offset[1] < b.offset[1] + b.size[1] + Expansion and \
+               a.offset[2] < b.offset[2] + b.size[2] + Expansion
     else:
-      Tolerance = 0.1 * max(a.size[0],b.size[0],a.size[1],b.size[1])
-      result = a.offset[0] + a.size[0] > b.offset[0] + Tolerance and \
-               a.offset[1] + a.size[1] > b.offset[1] + Tolerance and \
-               a.offset[0] + Tolerance < b.offset[0] + b.size[0] and \
-               a.offset[1] + Tolerance < b.offset[1] + b.size[1]
+      Expansion = -0.1 * min(a.size[0],b.size[0],a.size[1],b.size[1])
+      result = a.offset[0] + a.size[0] + Expansion > b.offset[0] and \
+               a.offset[1] + a.size[1] + Expansion > b.offset[1] and \
+               a.offset[0] < b.offset[0] + b.size[0] + Expansion and \
+               a.offset[1] < b.offset[1] + b.size[1] + Expansion
 
     return result
   
@@ -64,9 +69,15 @@ class ExtractFineGridFilter( Filter ):
     """
     
       Overwrite this one for the particular filter. 
+ 
+      We create an empty result array. Then we run over the input sequence
+      of cell_data. Per element, we check if a cell_data to the right within
+      this input sequence overlaps. 
       
       List operations are very epensive in Python. I therefore implement a 
-      two-pass stra
+      two-pass strategy, where I first sort the patches according to their
+      size. Actually, I sort them according to their inverted size, i.e. 
+      big entries come first.
       
     """
     def sort_key(patch):
@@ -108,13 +119,8 @@ class ExtractFineGridFilter( Filter ):
         ratio_for_print += delta_for_print
 
     if self.verbose:
-      print( "Copy results into output (commit changes)" )
-    result_cell_data = [None] * new_num_patches
-    for i in range(0,new_num_patches):
-      result_cell_data[i] = new_cell_data[i]
-
-    if self.verbose:
       print( "extracted " + str( new_num_patches ) + " from the " + str( len(cell_data) ) + " patch(es)" )
+    new_cell_data = new_cell_data[0:new_num_patches-1]
 
-    return result_cell_data, dof, dimensions, unknowns, description, mapping
+    return new_cell_data, dof, dimensions, unknowns, description, mapping
   
