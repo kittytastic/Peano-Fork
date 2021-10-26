7b5dab2751858e6b3fb8ade3285f402c59afd318
Marot <ga65jeg@mytum.de>
2021-10-26 15:55:50 +0200

    slight improvements on implementation files for ExaHype tutorial
    Former-commit-id: 3a5f6bd548a3dec036c2470b71c634ff77f16a0b

--- examples/exahype2/Exahype2_Tutorial/euler2D.cpp
+++ examples/exahype2/Exahype2_Tutorial/euler2D.cpp
@@ -12,55 +12,28 @@ void examples::exahype2::euler2D::euler2D::initialCondition(
   const tarch::la::Vector<Dimensions,double>&  volumeH,
   bool                                         gridIsConstructred
 ) {
-  logTraceInWith3Arguments( "initialCondition(...)", volumeCentre, volumeH, gridIsConstructred );
 
-  Q[0] = 0.1;  // rho
-  Q[1] = 0;    // momentum in x direction
-  Q[2] = 0;    // momentum in y direction
+	Q[0] = 0.1;  // rho
+	Q[1] = 0;    // momentum in x direction
+	Q[2] = 0;    // momentum in y direction
   
-  tarch::la::Vector<Dimensions,double> centre = {0.5, 0.5, 0.5};
-  bool isInTheCenter = ( tarch::la::norm2( volumeCentre-centre ) <= 0.15 );
-  
-  Q[3] = (isInTheCenter ? 1 : 0);     // energy
-
-  logTraceOut( "initialCondition(...)" );
+	bool isLeft = (volumeCentre[0] < 0.5);
+	Q[3] = (isLeft ? 1 : 0);
+//	tarch::la::Vector<Dimensions,double> centre = {0.5, 0.5, 0.5};
+//	bool isInTheCenter = ( tarch::la::norm2( volumeCentre-centre ) <= 0.15 );
+//	Q[3] = (isInTheCenter ? 1 : 0);     // energy
+	
 }
-
-
-
-
-
-void examples::exahype2::euler2D::euler2D::boundaryConditions(
-  const double * __restrict__                  Qinside, // Qinside[4+0]
-  double * __restrict__                        Qoutside, // Qoutside[4+0]
-  const tarch::la::Vector<Dimensions,double>&  faceCentre,
-  const tarch::la::Vector<Dimensions,double>&  volumeH,
-  double                                       t,
-  int                                          normal
-) {
-  logTraceInWith4Arguments( "boundaryConditions(...)", faceCentre, volumeH, t, normal );
-  
-  Qoutside[0] = Qinside[0];
-  Qoutside[1] = 0;
-  Qoutside[2] = 0;
-  Qoutside[3] = 0;
-  
-  logTraceOut( "boundaryConditions(...)" );
-}
-
-
-
-
 
 double ::examples::exahype2::euler2D::euler2D::maxEigenvalue(
-  const double * __restrict__ Q, // Q[4+0],
+  const double * __restrict__ Q, // Qma[4+0],
   const tarch::la::Vector<Dimensions,double>&  faceCentre,
   const tarch::la::Vector<Dimensions,double>&  volumeH,
   double                                       t,
   int                                          normal
 )  {
 	
-	  const double irho = 1./Q[0];
+	  const double irho = 1.0/Q[0];
 	  
 	  // based on the assumption that the fluid is an ideal gas, gamma chosen for dry air
 	  const double gamma = 1.4;  
@@ -77,16 +50,10 @@ double ::examples::exahype2::euler2D::euler2D::maxEigenvalue(
 		  result = std::max( std::abs(Q[2] * irho - c), std::abs(Q[2] * irho + c) );
 		  break;
 	  }
-
-	  
-	  logTraceOut( "maxEigenvalue(...)" );
 	  
 	  return result;
 
 }
-
-
-
 
 void ::examples::exahype2::euler2D::euler2D::flux(
   const double * __restrict__ Q, // Q[4+0],
@@ -104,8 +71,7 @@ void ::examples::exahype2::euler2D::euler2D::flux(
   const double gamma = 1.4;
   const double p = (gamma-1) * (Q[3] - 0.5*irho*(Q[1]*Q[1]+Q[2]*Q[2]));
   
-  switch(normal){
-  
+  switch(normal){  
   case 0: //in x direction
 	  F[0] = Q[1]; //rho
 	  F[1] = irho * Q[1] * Q[1] + p; 
@@ -120,14 +86,23 @@ void ::examples::exahype2::euler2D::euler2D::flux(
 	  break;
   }  
 
-  logTraceOut( "flux(...)" );
 }
 
-
-
-
-
-
+void examples::exahype2::euler2D::euler2D::boundaryConditions(
+  const double * __restrict__                  Qinside, // Qinside[4+0]
+  double * __restrict__                        Qoutside, // Qoutside[4+0]
+  const tarch::la::Vector<Dimensions,double>&  faceCentre,
+  const tarch::la::Vector<Dimensions,double>&  volumeH,
+  double                                       t,
+  int                                          normal
+) {
+  
+  Qoutside[0] = Qinside[0]; //density
+  Qoutside[1] = 0;			//velocity in x direction
+  Qoutside[2] = 0;			//velocity in y direction
+  Qoutside[3] = 0;			//Energy
+  
+}
 
 
 
--- examples/exahype2/Exahype2_Tutorial/swe.cpp
+++ examples/exahype2/Exahype2_Tutorial/swe.cpp
@@ -4,139 +4,141 @@
 
 tarch::logging::Log   examples::exahype2::swe::swe::_log( "examples::exahype2::swe::swe" );
 
+
+
+
+::exahype2::RefinementCommand examples::exahype2::swe::swe::refinementCriterion(
+  const double * __restrict__ Q, // Q[3+1]
+  const tarch::la::Vector<Dimensions,double>&  volumeX,
+  const tarch::la::Vector<Dimensions,double>&  volumeH,
+  double                                       t
+) {
+  ::exahype2::RefinementCommand result = ::exahype2::RefinementCommand::Keep;
+
+  if(tarch::la::equals(t, 0.0)){
+	  tarch::la::Vector<Dimensions,double> centre = {0.5, 0.5};
+	  bool nearCentre = tarch::la::norm2(volumeX-centre)>0.19 && tarch::la::norm2(volumeX-centre)<0.21;
+	  result = (nearCentre ? ::exahype2::RefinementCommand::Refine : ::exahype2::RefinementCommand::Coarsen);
+  }
+  return result;
+}
+
+
 void examples::exahype2::swe::swe::initialCondition(
   double * __restrict__ Q,
   const tarch::la::Vector<Dimensions,double>&  volumeCentre,
   const tarch::la::Vector<Dimensions,double>&  volumeH,
   bool                                         gridIsConstructred
 ) {
-  logTraceInWith3Arguments( "initialCondition(...)", volumeCentre, volumeH, gridIsConstructred );
 
+
+  tarch::la::Vector<Dimensions,double> centre = {0.5, 0.5};
+  bool nearCentre = tarch::la::norm2(volumeCentre-centre)<0.20;
   // height of water
-  bool isLeft = (volumeCentre[0] < 0.5);
-  Q[0] = (isLeft ? 1.0 : 0.5);
+  Q[0] = 1.0;
   
   // height times velocities
   Q[1] = 0;
   Q[2] = 0;
   
   // bathimetry
-  bool isExterior = (volumeCentre[1] < 0.25 || volumeCentre[1] > 0.75);
-  Q[3] = (isExterior ? 1.0 : 0.5);
+  Q[3] = (nearCentre ? 0.0 : 0.1);
+	
 
-  logTraceOut( "initialCondition(...)" );
 }
 
 
 void examples::exahype2::swe::swe::boundaryConditions(
-  const double * __restrict__                  Qinside, // Qinside[4+0]
-  double * __restrict__                        Qoutside, // Qoutside[4+0]
+  const double * __restrict__                  Qinside, // Qinside[3+1]
+  double * __restrict__                        Qoutside, // Qoutside[3+1]
   const tarch::la::Vector<Dimensions,double>&  faceCentre,
   const tarch::la::Vector<Dimensions,double>&  volumeH,
   double                                       t,
   int                                          normal
 ) {
-  logTraceInWith4Arguments( "boundaryConditions(...)", faceCentre, volumeH, t, normal );
-  
-  Qoutside[0] = Qinside[0];
-  Qoutside[1] = Qinside[1];
-  Qoutside[2] = Qinside[2];
-  Qoutside[3] = Qinside[3];
-	
-  logTraceOut( "boundaryConditions(...)" );
+	  Qoutside[0] = Qinside[0];
+	  Qoutside[1] = Qinside[1];
+	  Qoutside[2] = Qinside[2];
+	  Qoutside[3] = Qinside[3];
 }
 
 
 double ::examples::exahype2::swe::swe::maxEigenvalue(
-  const double * __restrict__ Q, // Q[4+0],
+  const double * __restrict__ Q, // Q[3+1],
   const tarch::la::Vector<Dimensions,double>&  faceCentre,
   const tarch::la::Vector<Dimensions,double>&  volumeH,
   double                                       t,
   int                                          normal
 )  {
-  logTraceInWith4Arguments( "maxEigenvalue(...)", faceCentre, volumeH, t, normal );
-
-  double result = 1.0;
-  double g = 9.81;
-  double c = std::sqrt( g * (Q[0]+Q[3]) );
-  double u = 0.0;
-  
-  switch(normal){
-  case 0: //x
-	  u = Q[0] * Q[1];
-	  result = std::max(u-c, u+c);
-	  break;
-  case 1: //y
-	  u = Q[0] * Q[2];
-	  result = std::max(u-c, u+c);
-	  break;
-  }
-  
-  return result;
-  
-  logTraceOut( "maxEigenvalue(...)" );
+	  double result = 1.0;
+	  double ih = 1/Q[0];
+	  double g = 9.81;
+	  double c = std::sqrt( g * (Q[0]+Q[3]) );
+	  double u = 0.0;
+	  
+	  switch(normal){
+	  case 0: //x
+		  u = ih * Q[1];
+		  result = std::max(u-c, u+c);
+		  break;
+	  case 1: //y
+		  u = ih * Q[2];
+		  result = std::max(u-c, u+c);
+		  break;
+	  }
+	  
+	  return result;
 }
 
 
 void ::examples::exahype2::swe::swe::flux(
-  const double * __restrict__ Q, // Q[4+0],
+  const double * __restrict__ Q, // Q[3+1],
   const tarch::la::Vector<Dimensions,double>&  faceCentre,
   const tarch::la::Vector<Dimensions,double>&  volumeH,
   double                                       t,
   int                                          normal,
-  double * __restrict__ F // F[4]
+  double * __restrict__ F // F[3]
 )  {
-  logTraceInWith4Arguments( "flux(...)", faceCentre, volumeH, t, normal );
-  
-  double irho = 1.0/Q[0];
-  
-  switch(normal){
-  case 0:
-	  F[0] = Q[1];
-	  F[1] = irho * Q[1] * Q[1];
-	  F[2] = irho * Q[1] * Q[2];
-	  F[3] = 0;
-	  break;
-  case 1:
-	  F[0] = Q[2];
-	  F[1] = irho * Q[2] * Q[1];
-	  F[2] = irho * Q[2] * Q[2];
-	  F[3] = 0;
-	  break;
-  }
-
-  logTraceOut( "flux(...)" );
+	  double ih = 1.0/Q[0];
+	  
+	  switch(normal){
+	  case 0:
+		  F[0] = Q[1];
+		  F[1] = ih * Q[1] * Q[1];
+		  F[2] = ih * Q[1] * Q[2];
+		  break;
+	  case 1:
+		  F[0] = Q[2];
+		  F[1] = ih * Q[2] * Q[1];
+		  F[2] = ih * Q[2] * Q[2];
+		  break;
+	  }
 }
 
 
 void ::examples::exahype2::swe::swe::nonconservativeProduct(
-  const double * __restrict__ Q, // Q[4+0],
-  const double * __restrict__             deltaQ, // [4+0]
+  const double * __restrict__ Q, // Q[3+1],
+  const double * __restrict__             deltaQ, // [3+1]
   const tarch::la::Vector<Dimensions,double>&  faceCentre,
   const tarch::la::Vector<Dimensions,double>&  volumeH,
   double                                       t,
   int                                          normal,
-  double * __restrict__ BgradQ // BgradQ[4]
+  double * __restrict__ BgradQ // BgradQ[3]
 )  {
-  logTraceInWith4Arguments( "nonconservativeProduct(...)", faceCentre, volumeH, t, normal );
-  
-  double g = 9.81;
-  
-  switch(normal){
-  case 0: //x
-	  BgradQ[0] = 0;
-	  BgradQ[1] = g * Q[0] * (deltaQ[0] + deltaQ[3]);
-	  BgradQ[2] = 0;
-	  BgradQ[3] = 0;
-	  break;
-  case 1: //y
-	  BgradQ[0] = 0;
-	  BgradQ[1] = 0;
-	  BgradQ[2] = g * Q[0] * (deltaQ[0] + deltaQ[3]);
-	  BgradQ[3] = 0;
-	  break;
-  }
-  
-  logTraceOut( "nonconservativeProduct(...)" );
+	  double g = 9.81;
+	  
+	  switch(normal){
+	  case 0: //x
+		  BgradQ[0] = 0.0;
+		  BgradQ[1] = g * Q[0] * (deltaQ[0] + deltaQ[3]);
+		  BgradQ[2] = 0.0;
+		  break;
+	  case 1: //y
+		  BgradQ[0] = 0.0;
+		  BgradQ[1] = 0.0;
+		  BgradQ[2] = g * Q[0] * (deltaQ[0] + deltaQ[3]);
+		  break;
+	  }
 }
+
 
