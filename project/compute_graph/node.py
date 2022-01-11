fb74abc564a00caae8224cc1692bca02ba8a914e
Jack Slingsby <jack@theslingsbys.com>
2022-01-11 18:41:30 +0000

    Re-wrote euler

--- project/compute_graph/node.py
+++ project/compute_graph/node.py
@@ -249,7 +249,21 @@ class Subtract(Node):
     def __repr__(self):
         return f"Sub-{super().__repr__()}"
 
+class Multiply(Node):
+    def __init__(self, num_inputs:int):
+        super().__init__(num_inputs, 1)
 
+    def _eval(self, inputs: List[Any])->List[Any]:
+        total = 1
+        for i in inputs:
+            total *= i
+        return [total]
+    
+    def visualize(self, dot: graphviz.Digraph):
+        dot.node(str(self.id), f"*") # type:ignore
+
+    def __repr__(self):
+        return f"Sub-{super().__repr__()}"
 
 def visualize_graph(g: Graph, out_path:str="Artifacts", out_file_name:str="tmp"):
     dot = graphviz.Digraph()
@@ -264,37 +278,57 @@ def visualize_graph(g: Graph, out_path:str="Artifacts", out_file_name:str="tmp")
 
 
 
-'''
+
 def Euler2D_X()->Graph:
     g = Graph(4,4)
-    p = PlaceHolderNode("p")
-    irho = PlaceHolderNode("irho")
-    g.add_edge(g.inputs[0], p)
-    g.add_edge(g.inputs[1], p)
-    g.add_edge(g.inputs[2], p)
-    g.add_edge(g.inputs[3], p)
+    p = Multiply(4)
+    irho = PassThroughNode(name="irho")
 
-    g.add_edge(g.inputs[0], irho)
-    
-    g.add_edge(irho, g.outputs[1])
-    g.add_edge(irho, g.outputs[2])
-    g.add_edge(irho, g.outputs[3])
+    mul1 = Multiply(3)
+    mul2 = Multiply(3)
+    mul3 = Multiply(3)
 
-    g.add_edge(p, g.outputs[2])
-    g.add_edge(p, g.outputs[3])
+    add1 = Add(2)
+    add2 = Add(2)
 
-    g.add_edge(g.inputs[1], g.outputs[1])
+    # To P
+    g.add_edge(g.get_internal_input(0), (p,0))
+    g.add_edge(g.get_internal_input(1), (p,1))
+    g.add_edge(g.get_internal_input(2), (p,2))
+    g.add_edge(g.get_internal_input(3), (p,3))
 
-    g.add_edge(g.inputs[2], g.outputs[0])
-    g.add_edge(g.inputs[2], g.outputs[1])
-    g.add_edge(g.inputs[2], g.outputs[2])
-    g.add_edge(g.inputs[2], g.outputs[3])
+    # irho
+    g.add_edge(g.get_internal_input(0), (irho, 0))
 
-    g.add_edge(g.inputs[3], g.outputs[3])
+
+    # F[0]
+    g.add_edge(g.get_internal_input(2), g.get_internal_output(0))
+
+    # F[1]
+    g.add_edge((irho,0), (mul1, 0))
+    g.add_edge(g.get_internal_input(1), (mul1, 1))
+    g.add_edge(g.get_internal_input(1), (mul1, 2))
+    g.add_edge((mul1, 0), (add1, 0))
+    g.add_edge((p, 0), (add1, 1))
+    g.add_edge((add1, 0), g.get_internal_output(1))
+
+    # F[2]
+    g.add_edge((irho,0), (mul2, 0))
+    g.add_edge(g.get_internal_input(1), (mul2, 1))
+    g.add_edge(g.get_internal_input(2), (mul2, 2))
+    g.add_edge((mul2, 0), g.get_internal_output(2))
+
+    # F[3]
+    g.add_edge((irho,0), (mul3, 0))
+    g.add_edge(g.get_internal_input(1), (mul3, 1))
+    g.add_edge(g.get_internal_input(3), (add2, 0))
+    g.add_edge((p, 0), (add2, 1))
+    g.add_edge((add2, 0), (mul3, 2))
+    g.add_edge((mul3, 0), g.get_internal_output(3))
+
     return g
-'''
 
-if __name__=="__main__":
+def basic_graph()->Graph:
     g=Graph(2,3)
     add = Add(2)
     sub = Subtract()
@@ -307,10 +341,16 @@ if __name__=="__main__":
     g.add_edge(g.get_internal_input(0), (sub,0))
     g.add_edge(g.get_internal_input(1), (sub,1))
     g.add_edge((sub,0), g.get_internal_output(2))
+
+    return g
+
+if __name__=="__main__":
+
+    g = Euler2D_X()    
     e_msg = '\n'.join([e for e in g.validate()])
     print(f"Errors:\n{e_msg}")
     #g = Euler2D_X()
     visualize_graph(g)
 
-    outputs = g.eval([1,2])
-    print(f"Output: {outputs}")
+    #outputs = g.eval([1,2])
+    #print(f"Output: {outputs}")
