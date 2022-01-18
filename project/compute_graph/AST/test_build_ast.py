f7b30abf1130c3f4efc67990dc39a25c61685c9e
Jack Slingsby <jack@theslingsbys.com>
2022-01-18 16:31:34 +0000

    Recovers broken repo - pt2/2

---
+++ project/compute_graph/AST/test_build_ast.py
@@ -0,0 +1,20 @@
+import unittest
+from build_ast import *
+from parameterized import parameterized
+
+class TestAttributeParsing(unittest.TestCase):
+    @parameterized.expand([ # type:ignore
+       ("var:Type", ("var", AttrTypes.SINGLE, "Type")),
+       ("var*:Type", ("var", AttrTypes.LIST, "List[Type]")),
+       ("var**:Type", ("var", AttrTypes.LOL, "List[List[Type]]")),
+       ("var", ("var", AttrTypes.SINGLE, "Any")),
+       ("var*", ("var", AttrTypes.LIST, "List[Any]")),
+       ("var**", ("var", AttrTypes.LOL, "List[List[Any]]")),
+    ])
+    def test(self, arg:str, expected: Optional[CFG_Attribute]):
+        self.assertEqual(parse_cfg_attribute(arg), expected)
+
+
+
+if __name__=="__main__":
+    unittest.main()
