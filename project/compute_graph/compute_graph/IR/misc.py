fec37ec67a5d66135ea21931a21019daa83324af
Jack Slingsby <jack@theslingsbys.com>
2022-01-30 17:34:38 +0000

    Finish in, out inline

--- project/compute_graph/compute_graph/IR/misc.py
+++ project/compute_graph/compute_graph/IR/misc.py
@@ -6,7 +6,14 @@ from compute_graph.IR.transform import IR_Transfrom
 class InlineInOut(IR_Transfrom):
     def tf(self, in_IR: IR_Symbol)->IR_Symbol:
         working_ir = self.assert_and_cast_symbol_type(in_IR, IR_Function) 
+        working_ir = self.inline_inputs(working_ir)
+        print(working_ir)
+        working_ir = self.inline_outputs(working_ir)
+        
 
+        return working_ir
+
+    def inline_inputs(self, working_ir: IR_Function)->IR_Function:
         in_vars = working_ir.in_var
         invar_alias: Dict[IR_Variable, IR_Variable] = {}
         delete_lines:List[int] = []
@@ -17,28 +24,36 @@ class InlineInOut(IR_Transfrom):
                     invar_alias[l.expr] = l.assign_var
                     delete_lines.append(idx)
 
-        print(invar_alias)
         for idx in reversed(delete_lines):
             del working_ir.body[idx]
-        
-        #print("delete assigns")
-        #print(str(in_IR))
 
         for iv, alias in invar_alias.items():
-            to_fix: List[bool] = []
             for l in working_ir.body:
-                is_in = alias in l
-                to_fix.append(is_in) 
-            print(f"{alias}->{iv}: {to_fix}")
-
-            replace_c:List[int] = [] 
-            for l in working_ir.body:
-                replace_c.append(l.replace(alias, iv))
+                l.replace(alias, iv)
             
-            print(f"       :{replace_c}")
-
-        return working_ir
         
+        return working_ir
 
 
+    def inline_outputs(self, working_ir: IR_Function)->IR_Function:
+        out_vars = working_ir.out_var
+        outvar_alias: Dict[IR_Variable, IR_Variable] = {}
+        delete_lines:List[int] = []
+
+        for idx, l in enumerate(working_ir.body):
+            if isinstance(l, IR_SingleAssign):
+                if l.assign_var in out_vars:
+                    assert(isinstance(l.expr, IR_Variable))
+                    outvar_alias[l.assign_var] = l.expr
+                    delete_lines.append(idx)
+
+        for idx in reversed(delete_lines):
+            del working_ir.body[idx]
+
+        for iv, alias in outvar_alias.items():
+            for l in working_ir.body:
+               l.replace(alias, iv)
+            
+        
+        return working_ir
     
