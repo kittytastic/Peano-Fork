aeb8412678cbdf04b1b04ac9235255b916d55d10
Jack Slingsby <jack@theslingsbys.com>
2022-02-01 17:44:20 +0000

    Tests and fixes IR_Symbol replace

--- project/compute_graph/compute_graph/IR/symbols.py
+++ project/compute_graph/compute_graph/IR/symbols.py
@@ -1,4 +1,4 @@
-from typing import List, Union, Tuple, Any, Optional
+from typing import List, Union, Tuple, Any, Optional, cast
 from enum import Enum
 import inspect
 
@@ -12,12 +12,17 @@ class IR_Symbol():
         attributes = [a for a in attributes if not(a[0].startswith('__') and a[0].endswith('__'))]
         return attributes
 
-    def _is_list_of_symbol(self, in_list:List[Any])->Optional[List['IR_Symbol']]:
+    def _is_list_of_symbol(self, in_list:Any)->Optional[List['IR_Symbol']]:
+        if not isinstance(in_list, list):
+            return None
+        
+        in_list = cast(List[Any], in_list)
         for i in in_list:
             if not isinstance(i, IR_Symbol):
                 return None
         
         return in_list
+    
 
     def __contains__(self, key:Union['IR_Symbol', type]):
         if inspect.isclass(key):
@@ -30,28 +35,36 @@ class IR_Symbol():
         attributes = self._get_all_attributes()
         child_ins:List[bool] = [key in val for _, val in attributes if isinstance(val, IR_Symbol)]
         
-        child_lists: List[List[Any]] = [val for _,val in attributes if isinstance(val, list)]
-        child_sym_lists:List[List['IR_Symbol']] = [l for l in child_lists if self._is_list_of_symbol(l)]
+        child_sym_lists:List[List['IR_Symbol']] = [l for _,l in attributes if self._is_list_of_symbol(l)]
         child_list_ins: List[bool] = [key in i for l in child_sym_lists for i in l]
 
         return any(child_ins) or any(child_list_ins)
     
     def replace(self, find:'IR_Symbol', replace: 'IR_Symbol')->int:
-        # TODO wont work with literal attributes
         if find == self:
-            raise Exception("Cannot replace. The symbol yoy called replace on needs to be replaced")
+            raise Exception("Cannot replace. The symbol you called replace on needs to be replaced")
 
-        attributes = inspect.getmembers(self, lambda a:not(inspect.isroutine(a)))
-        attributes = [a for a in attributes if not(a[0].startswith('__') and a[0].endswith('__'))]
-        child_symbol = [(k,v) for k, v in attributes if isinstance(v, IR_Symbol)]
+        attributes = self._get_all_attributes() 
+        child_symbols = [(k,v) for k, v in attributes if isinstance(v, IR_Symbol)]
+        child_lists:List[Tuple[str,List['IR_Symbol']]] = [(k,l) for k,l in attributes if self._is_list_of_symbol(l)]
 
         replacement_count = 0
-        for k, s in child_symbol:
+        for k, s in child_symbols:
             if s == find:
                 setattr(self, k, replace)
                 replacement_count +=1
             else:
                 replacement_count += s.replace(find, replace)
+
+        for k, l in child_lists:
+            l_ref = getattr(self, k)
+            for idx, v in enumerate(l):
+                if v==find:
+                    l_ref[idx] = replace
+                    replacement_count +=1
+                else:
+                    replacement_count += v.replace(find, replace)
+
 
         return replacement_count
 
