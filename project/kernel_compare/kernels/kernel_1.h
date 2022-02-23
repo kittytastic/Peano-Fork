7ebc79ce4af6cbce5fc00f1f641b6f78ae569296
Jack Slingsby <jack@theslingsbys.com>
2022-02-23 04:00:25 +0000

    Begin tidy

--- project/kernel_compare/kernels/kernel_1.h
+++ project/kernel_compare/kernels/kernel_1.h
@@ -1,12 +1,11 @@
-#pragma once
-#include "kernel_base.h"
-#include "kernel_2_base.h"
-#include "data_generator.h"
+#include "../kernel_base.h"
+#include "kernel_1_base.h"
+#include "../data/data_generator.h"
 #include "tarch/la/Vector.h"
 #include  <iostream>
-#include "euler_data.h"
+#include "../data/euler_data.h"
 
-void k2_getdata(int x, double* data){
+void k1_getdata(int x, double* data){
     for(int i=0; i<x; i++){
         data[i] = in_euler2d_1[i];
     }
@@ -15,14 +14,14 @@ void k2_getdata(int x, double* data){
     //for(int i=0; i<10000000; i++){b++;}
 }
 
-void k2_runKernel(double * inVec, double* outVec){
+void k1_runKernel(double * inVec, double* outVec){
     tarch::la::Vector<2,double> patchCenter;
     patchCenter(0)=1;
     patchCenter(1)=1;
     tarch::la::Vector<2,double> patchSize;
     patchSize(0)=1;
     patchSize(1)=1;
-    compliedKernel(
+    kernels::k1::kernelLambda(
         patchCenter,
         patchSize,
         1.0,
@@ -30,20 +29,21 @@ void k2_runKernel(double * inVec, double* outVec){
         inVec,
         outVec
     );
+    
 }
 
-std::vector<testCase> k2_tests = {
+std::vector<testCase> k1_tests = {
     testCase("Basic", in_euler2d_1, out_euler2d_1)
     ,testCase("Exciting 1", in_euler2d_2, out_euler2d_2)
     ,testCase("Exciting 2", in_euler2d_3, out_euler2d_3)
     };
 
-Kernel k2(
-    "Kernel 2: Basic Inline",
-    k2_getdata,
-    k2_runKernel,
+Kernel k1(
+    "Kernel 1: Base Euler",
+    k1_getdata,
+    k1_runKernel,
     5*5*4,
     3*3*4,
-    k2_tests
+    k1_tests
     );
 
