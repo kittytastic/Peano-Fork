1ecb3a7138e24dafae919768e3a2b07e5186a1e9
Jack Slingsby <jack@theslingsbys.com>
2022-03-02 12:51:37 +0000

    Adds cached symbol attr

--- project/compute_graph/compute_graph/IR/symbols/symbol_base.py
+++ project/compute_graph/compute_graph/IR/symbols/symbol_base.py
@@ -11,16 +11,75 @@ class IR_DataTypes(Enum):
     FP64 = 1
 
 class IR_Symbol(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+        self._cached_attributes:Optional[Tuple[Set[str], Set[str], Set[str]]]=None
+
+
     def _get_all_attributes(self)->List[Tuple[str, Any]]:
         attributes = inspect.getmembers(self, lambda a:not(inspect.isroutine(a)))
         attributes = [a for a in attributes if not(a[0].startswith('__') and a[0].endswith('__'))]
         return attributes
+    
+    '''def _asd(self)->List[Tuple[str, Any]]:
+        # !!! DONT CACHE VALUES !!!
+        if not hasattr(self, "_cached_attributes"):
+            self._cached_attributes = None
+        
+        if self._cached_attributes is None:
+            self._cached_attributes = self._generate_attributes()
+        
+        test_gen = self._generate_attributes()
+        if test_gen!=self._cached_attributes:
+            print(f"True: {test_gen}")
+            print(f"Cached: {self._cached_attributes}")
+
+            #raise Exception()
+        return self._cached_attributes
+        #return self._generate_attributes()
+    '''
+
+    def _enforce_property_creation(self):
+        if not hasattr(self, "_cached_attributes"):
+            self._cached_attributes = None
+         
+
+    def _get_sub_symbol_keys(self)->Tuple[Set[str], Set[str]]:
+        self._enforce_property_creation()
+        
+        if self._cached_attributes is None:
+            attribute_keys = [a for a, _ in self._get_all_attributes()]
+            single_keys = [k for k in attribute_keys if isinstance(getattr(self, k), IR_Symbol)]
+            multi_keys = [k for k in attribute_keys if self._is_list_of_symbol(getattr(self, k))]
+            unknown_keys = [k for k in attribute_keys if isinstance(getattr(self, k), list) and len(getattr(self, k))==0]
+
+            self._cached_attributes = (set(single_keys), set(multi_keys), set(unknown_keys))
+        
+        rs:Set[str] = set()
+        for uk in self._cached_attributes[2]:
+            uk_v = getattr(self, uk)
+            assert(isinstance(uk_v, list))
+            uk_v = cast(List[Any], uk_v)
+
+            if len(uk_v)>0:
+                rs.add(uk)
+                
+                if self._is_list_of_symbol(uk_v) is not None:
+                    self._cached_attributes[1].add(uk)
+        
+        for r in rs:
+            self._cached_attributes[2].remove(r)
+                
+        return (self._cached_attributes[0], self._cached_attributes[1])
 
     def _is_list_of_symbol(self, in_list:Any)->Optional[List['IR_Symbol']]:
         if not isinstance(in_list, list):
             return None
         
         in_list = cast(List[Any], in_list)
+        if len(in_list)==0:
+            return None
+        
         for i in in_list:
             if not isinstance(i, IR_Symbol):
                 return None
