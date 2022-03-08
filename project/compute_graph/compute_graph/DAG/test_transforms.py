dd4c9b6ad18593439e70d32253c18832ac146857
Jack Slingsby <jack@theslingsbys.com>
2022-03-08 18:22:07 +0000

    Cuts down testcase

--- project/compute_graph/compute_graph/DAG/test_transforms.py
+++ project/compute_graph/compute_graph/DAG/test_transforms.py
@@ -1,6 +1,6 @@
 from typing import Callable
 import unittest
-from compute_graph.DAG.graph import Graph
+from compute_graph.DAG.graph import DAG_Message, Graph
 from compute_graph.DAG.ops import Multiply, Max, Subtract, Add
 from compute_graph.DAG.primitive_node import Constant
 from compute_graph.DAG.transform import DAG_Flatten
@@ -9,7 +9,7 @@ from compute_graph.DAG.visualize import visualize_graph
 class TestFlatten(unittest.TestCase):
     def xtest_1(self):
         tf = DAG_Flatten()
-        g_top_top = Graph(2, 1, "top_top")
+        g_top_top = Graph(3, 1, "top_top")
         g_top = Graph(2, 1, "top")
         g_bottom = Graph(2, 1, "bottom")
 
@@ -27,13 +27,17 @@ class TestFlatten(unittest.TestCase):
         g_flat = tf.tf(g_top_top)
 
         visualize_graph(g_flat, out_path="../Artifacts", out_file_name="test_after")
+        
+        _, sub_graph = g_flat.get_categoriesed_sub_nodes()
+        self.assertEqual(len(sub_graph), 0)
     
     def test_2(self):
-        g = test_rusanov(4, proper_max_eigen_x, proper_flux_x)
+        g = test_rusanov(2, proper_max_eigen_x, proper_flux_x)
 
         tf = DAG_Flatten()
         visualize_graph(g, out_path="../Artifacts", out_file_name="test_before")
 
+        DAG_Message.print_summary(g.validate())
         g_flat = tf.tf(g)
 
         visualize_graph(g_flat, out_path="../Artifacts", out_file_name="test_after")
@@ -48,13 +52,8 @@ def test_rusanov(
     flux_builder: Callable[[], Graph],
     friendly_name:str="rusanov")->Graph:
 
-    # Q left (unknowns)
-    # Q right (unknowns)
-    # patch center (2)
-    # patch dx     (1)
-    # t            (1)
-    # dt           (1)
-    g = Graph(unknowns*2 + 2+1+1+1, unknowns*2, friendly_name)
+  
+    g = Graph(unknowns*2 + 1, unknowns*2, friendly_name)
     flux_l = flux_builder()
     flux_r = flux_builder()
 
@@ -68,7 +67,7 @@ def test_rusanov(
         g.add_edge(g.get_internal_input(u), eigen_l.get_external_input(u))
         g.add_edge(g.get_internal_input(unknowns + u), eigen_r.get_external_input(u))
 
-    for e in range(2+1+1+1):
+    for e in range(1):
         g.add_edge(g.get_internal_input(unknowns+e), flux_l.get_external_input(unknowns+e))
         g.add_edge(g.get_internal_input(unknowns+e), eigen_l.get_external_input(unknowns+e))
         g.add_edge(g.get_internal_input(unknowns+e), flux_r.get_external_input(unknowns+e))
@@ -78,25 +77,12 @@ def test_rusanov(
     g.fill_node_inputs([eigen_l, eigen_r], l_max)
 
     for i in range(unknowns):
-        h = Constant(0.5)
-        mul1 = Multiply(2)
         mul2 = Multiply(2)
-        mul3 = Multiply(3)
-        sub1 = Subtract()
         sub2 = Subtract()
-        add1 = Add(2)
 
-        # QR - QL
-        g.fill_node_inputs([g.get_internal_input(i+unknowns), g.get_internal_input(i)], sub1)
-        # 0.5 * lmax * (...)
-        g.fill_node_inputs([h, l_max, sub1], mul3)
+        g.fill_node_inputs([(flux_l, i), (flux_r, i)], mul2)
 
-        # 0.5 * FL + 0.5 * FR
-        g.fill_node_inputs([h, (flux_l, i)], mul1)
-        g.fill_node_inputs([h, (flux_r, i)], mul2)
-        g.fill_node_inputs([mul1, mul2], add1)
-
-        g.fill_node_inputs([add1, mul3], sub2)
+        g.fill_node_inputs([mul2, l_max], sub2)
 
         g.add_edge((sub2, 0), g.get_internal_output(i))
         g.add_edge((sub2, 0), g.get_internal_output(i+unknowns))
@@ -104,13 +90,13 @@ def test_rusanov(
     return g
 
 def proper_flux_x()->Graph:
-    g = Graph(4+2+1+1+1, 4, "proper-flux-x")
-    for i in range(4):
+    g = Graph(2+1, 2, "proper-flux-x")
+    for i in range(2):
         g.add_edge(g.get_internal_input(i), g.get_internal_output(i))
     return g
 
 def proper_max_eigen_x()->Graph:
-    g = Graph(4+2+1+1+1, 1, "proper-max-eigen-x")
+    g = Graph(2+1, 1, "proper-max-eigen-x")
     g.add_edge(g.get_internal_input(0), g.get_internal_output(0))
     
     return g
