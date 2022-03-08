801c495ff26f4d7822c9a4f4c4ba1930d54abcfe
Jack Slingsby <jack@theslingsbys.com>
2022-03-08 18:53:10 +0000

    Fixes flatten bug

--- project/compute_graph/compute_graph/DAG/test_transforms.py
+++ project/compute_graph/compute_graph/DAG/test_transforms.py
@@ -1,14 +1,9 @@
-from typing import Callable
 import unittest
-from compute_graph.DAG.graph import DAG_Message, Graph
-from compute_graph.DAG.ops import Multiply, Max, Subtract, Add
-from compute_graph.DAG.primitive_node import Constant
+from compute_graph.DAG.graph import Graph
 from compute_graph.DAG.transform import DAG_Flatten
-from compute_graph.DAG.visualize import visualize_graph
 
 class TestFlatten(unittest.TestCase):
     def test_1(self):
-        print("Hello")
         tf = DAG_Flatten()
         g_top = Graph(1, 1, "top")
         g_bottom = Graph(2, 1, "bottom")
@@ -18,13 +13,7 @@ class TestFlatten(unittest.TestCase):
         g_top.add_edge(g_top.get_internal_input(0), g_bottom.get_external_input(1))
         g_top.add_edge(g_bottom.get_external_output(0), g_top.get_internal_output(0))
 
-        
-        visualize_graph(g_top, out_path="../Artifacts", out_file_name="test_before_flat")
-        visualize_graph(g_top, out_path="../Artifacts", out_file_name="test_before", max_depth=1)
-
         g_flat = tf.tf(g_top)
-
-        visualize_graph(g_flat, out_path="../Artifacts", out_file_name="test_after")
         
         _, sub_graph = g_flat.get_categoriesed_sub_nodes()
         self.assertEqual(len(sub_graph), 0)
