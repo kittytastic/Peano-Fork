7b8cc9baef29f1a06bb58c9b358a59d11bad15a4
Jack Slingsby <jack@theslingsbys.com>
2022-03-11 19:31:34 +0000

    Made the tests 3d friendly

--- project/kernel_compare/kernels/kernel_base.h
+++ project/kernel_compare/kernels/kernel_base.h
@@ -4,15 +4,19 @@
 struct testCase{
     testCase(std::string name, double cellPos0,
     double cellPos1,
+    double cellPos2,
     double cellSize0,
     double cellSize1,
+    double cellSize2,
     double t,
     double dt, double * input, double * expected):
     name(name),
     cellPos0(cellPos0),
     cellPos1(cellPos1),
+    cellPos2(cellPos2),
     cellSize0(cellSize0),
     cellSize1(cellSize1),
+    cellSize2(cellSize2),
     t(t),
     dt(dt),
     input(input),
@@ -21,8 +25,10 @@ struct testCase{
     std::string name;
     double cellPos0;
     double cellPos1;
+    double cellPos2;
     double cellSize0;
     double cellSize1;
+    double cellSize2;
     double t;
     double dt;
     double * input;
