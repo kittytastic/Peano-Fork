dc190015060e564ec23605546ab0f216ac5ee519
Jack Slingsby <jack@theslingsbys.com>
2022-03-22 21:24:14 +0000

    Fixes some make bugs

--- project/exahype/MakeAll.sh
+++ project/exahype/MakeAll.sh
@@ -1,7 +1,7 @@
 echo "------ Preparing Projects ------"
 (cd Euler2D/default_kernel; git clean -xdf; python euler2D.py; make clean;)>>/dev/null
 echo "Euler 2D - default: DONE"
-(cd Euler2D/generated; git clean -xdf; python euler2D.py; make clean; git restore observers/TimeStep2peano4_toolbox_blockstructured_ReconstructPatchAndApplyFunctor5.cpp; python ../../fix_makefile.py swe.g.cpp)>>/dev/null
+(cd Euler2D/generated; git clean -xdf; python euler2D.py; make clean; git restore observers/TimeStep2peano4_toolbox_blockstructured_ReconstructPatchAndApplyFunctor5.cpp; python ../../fix_makefile.py euler2d.g.cpp)>>/dev/null
 echo "Euler 2D - generated: DONE"
 (cd SWE/default_kernel; git clean -xdf; python SWE.py; make clean; git restore observers/TimeStep2peano4_toolbox_blockstructured_ReconstructPatchAndApplyFunctor5.cpp)>>/dev/null
 echo "SWE - default: DONE"
